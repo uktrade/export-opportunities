cc7c198ea36c7035b98a0e8133692823ed5e6333
Dermot Byrne <dermotbyrne@dermot.local>
2017-04-25 18:15:55 +0100

    More styling and tweaking search filter logic

--- app/assets/javascripts/start-page.js
+++ app/assets/javascripts/start-page.js
@@ -5,14 +5,31 @@ if(filtersEl) {
 	ukti.SearchFilters.init(filtersEl);
 }
 
-if ($( ".select-custom" ).length) {
-	$( ".select-custom" ).select2({
-		theme: "flat",
+if ($('.select-custom').length) {
+
+	$('.select-custom').select2({
+		theme: 'flat',
+		selectOnClose: true,
 		escapeMarkup: function(markup) {
 		  return markup;
 		},
 		templateResult: function(result) {
 			return '<svg class="icon icon-check"><use xlink:href="#icon-ditcheckmark" /></svg>' + result.text;
     }
-	});	
+	});
+
+	/* prevent dropdown from opening when deselecting tag */
+	$('.select-custom').on('select2:unselecting', function(e) {
+	    $(this).on('select2:opening', function(e) {
+	        e.preventDefault();
+	    });
+
+	});
+
+	$('.select-custom').on('select2:unselect', function(e) {
+	     var sel = $(this);
+	     setTimeout(function() {
+	       sel.off('select2:opening');
+	     }, 1);
+	});
 }
