4add07577fca0c93300f6c69187e1ce6c187a23b
alex giamas <agiamas@gmail.com>
2017-04-26 11:51:33 +0100

    (fix) title is a link

--- app/mailers/enquiry_feedback_mailer.rb
+++ app/mailers/enquiry_feedback_mailer.rb
@@ -3,6 +3,6 @@ class EnquiryFeedbackMailer < ApplicationMailer
   def request_feedback(enquiry_feedback)
     @enquiry_feedback = enquiry_feedback
 
-    mail(to: @enquiry_feedback.enquiry.email, subject: 'What happened with your export opportunity #{@enquiry_feedback.enquiry.opportunity.title}?')
+    mail(to: @enquiry_feedback.enquiry.email, subject: 'What happened with your export opportunity ' + @enquiry_feedback.enquiry.opportunity.title + '?')
   end
 end
