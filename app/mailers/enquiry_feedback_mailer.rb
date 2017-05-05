77855f984c47273d125e293f9f14d9a8994fc924
Dermot Byrne <dermotbyrne@dermot.local>
2017-05-05 15:15:40 +0100

    (fix) updating email subject line again

--- app/mailers/enquiry_feedback_mailer.rb
+++ app/mailers/enquiry_feedback_mailer.rb
@@ -3,6 +3,6 @@ class EnquiryFeedbackMailer < ApplicationMailer
   def request_feedback(enquiry_feedback)
     @enquiry_feedback = enquiry_feedback
 
-    mail(from: 'Export opportunities', to: @enquiry_feedback.enquiry.email, subject: 'Give feedback on #{@enquiry_feedback.enquiry.opportunity.title}')
+    mail(from: 'Export opportunities', to: @enquiry_feedback.enquiry.email, subject: 'Give feedback on #{@enquiry_feedback.enquiry.opportunity.title}?')
   end
 end
