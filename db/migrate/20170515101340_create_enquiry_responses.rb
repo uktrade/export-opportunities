5cc2775f126eca78b5e1fef2e6e119e6f22e9d03
Dermot Byrne <dermotbyrne@dermot.local>
2017-05-25 14:23:10 +0100

    (fix) fixing up grid a bit

--- db/migrate/20170515101340_create_enquiry_responses.rb
+++ db/migrate/20170515101340_create_enquiry_responses.rb
@@ -2,9 +2,10 @@ class CreateEnquiryResponses < ActiveRecord::Migration
   def change
     create_table :enquiry_responses do |t|
       t.belongs_to :editor, index: true, foreign_key: true
-      t.belongs_to :enquiry, index: true, foreign_key: true
+      t.references :enquiry, index: true
       t.text :email_body
       t.timestamps null: false
     end
+    add_foreign_key :enquiry, :enquiry_responses
   end
 end
