9a3d5c70710c772758cc2a99943256409c3d2eae
alex giamas <agiamas@gmail.com>
2017-07-12 16:29:27 +0100

    (fix) correct options in email going out, responses being recorded, feedback opt out works

--- app/controllers/enquiry_feedback_controller.rb
+++ app/controllers/enquiry_feedback_controller.rb
@@ -2,10 +2,8 @@ class EnquiryFeedbackController < ApplicationController
   rescue_from EncryptedParams::CouldNotDecrypt, with: :not_found
 
   def new
-    # @enquiry_feedback = EnquiryFeedback.find(enquiry_feedback_params[:id])
+    @enquiry_feedback = EnquiryFeedback.find(enquiry_feedback_params[:id])
     @enquiry_feedback = EnquiryFeedback.first
-
-    
 
     if @enquiry_feedback.responded_at.nil?
 
@@ -13,9 +11,9 @@ class EnquiryFeedbackController < ApplicationController
         initial_response: enquiry_feedback_params[:response],
         responded_at: Time.zone.now
       )
-      @responseText = response_display_text[enquiry_feedback_params[:response]]
     end
 
+    @response_text = response_display_text(enquiry_feedback_params[:response])
     render 'enquiry_feedback/new'
   end
 
@@ -35,8 +33,8 @@ class EnquiryFeedbackController < ApplicationController
     @_enquiry_feedback_params ||= EncryptedParams.decrypt(params[:q])
   end
 
-  def response_display_text
-    text = { :won => 'Won', :did_not_win => 'Did not' }
-    return text['won']
+  def response_display_text(response)
+    text = { won: 'Won :)', did_not_win: 'Did not win :( ', never_heard_back: 'Never heard back :O', dontknow_want_to_say: 'I dont know or want to say', no_response: 'I got no response back' }
+    text[response]
   end
 end
--- app/models/enquiry_feedback.rb
+++ app/models/enquiry_feedback.rb
@@ -1,12 +1,10 @@
 class EnquiryFeedback < ActiveRecord::Base
   belongs_to :enquiry
   enum initial_response: {
-    no_response: 0,
-    won: 1,
-    did_not_win: 2,
-    dont_know_outcome: 3,
-    never_heard_back: 4,
-    cannot_remember: 5,
-    dontknow_want_to_say: 6,
+    dont_know_outcome: 0,
+    did_not_win: 1,
+    won: 2,
+    no_response: 3,
+    dontknow_want_to_say: 4,
   }
 end
