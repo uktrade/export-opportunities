46981fd04241aa369444fffa43ad04f932520b55
alex giamas <agiamas@gmail.com>
2017-07-18 21:16:11 +0100

    (fix) spec with updated functionality

--- spec/controllers/enquiry_feedback_controller_spec.rb
+++ spec/controllers/enquiry_feedback_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe EnquiryFeedbackController do
       expect(feedback.responded_at).not_to be_nil
     end
 
-    it 'does not update the enquiry feedback record more than once' do
+    it 'updates the enquiry feedback record with every new click' do
       first_responded_at = 1.month.ago.round
       feedback = create(
         :enquiry_feedback,
@@ -28,8 +28,8 @@ RSpec.describe EnquiryFeedbackController do
 
       feedback.reload
 
-      expect(feedback).to be_won
-      expect(feedback.responded_at).to eql first_responded_at
+      expect(feedback).to be_did_not_win
+      expect(feedback.responded_at).to_not eql first_responded_at
     end
 
     it 'provides an friendly but uninformative error if invalid' do
