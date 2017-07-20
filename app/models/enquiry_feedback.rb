4df54023a4516bb71c02b87f819a4e76beeb3987
Dermot Byrne <appility@users.noreply.github.com>
2017-07-20 10:07:39 +0100

    (hotfix) changing great.gov.uk to opportunities.great.gov.uk (#51)
    * (hotfix) changing great.gov.uk to opportunities.great.gov.uk
    
    * (fix) aligning impact email enquiry options with model
    
    * (fix) spec with updated functionality

--- app/models/enquiry_feedback.rb
+++ app/models/enquiry_feedback.rb
@@ -1,10 +1,10 @@
 class EnquiryFeedback < ActiveRecord::Base
   belongs_to :enquiry
   enum initial_response: {
-    dont_know_outcome: 0,
+    won: 0,
     did_not_win: 1,
-    won: 2,
-    no_response: 3,
-    dontknow_want_to_say: 4,
+    dont_know_outcome: 2,
+    dontknow_want_to_say: 3,
+    no_response: 4,
   }
 end
