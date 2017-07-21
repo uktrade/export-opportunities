ea3977047fb6118538a1685226c98d623c5599a8
Dermot Byrne <dermotbyrne@dermot.local>
2017-07-21 16:30:16 +0100

    (feature) showing error if you dont add comment when returning to draft

---
+++ app/assets/javascripts/components/OpportunityManagement.js
@@ -0,0 +1,64 @@
+
+var ukti = window.ukti || {};
+
+ukti.OpportunityManagement = (function() {
+  'use strict';
+
+	var commentFormGroup = null,
+			commentFormErrorEl = null,
+			commentField = null,
+			errorClass = 'form-group-error',
+			errorMessage = 'Please add a comment to be sent to the original uploader before returning opportunity to draft state';
+
+  var cacheElements = function () {
+		var commentForm = document.getElementById('new_opportunity_comment_form');
+		commentField = commentForm.querySelector('textarea[name="opportunity_comment_form[message]"]');
+		commentFormGroup = commentForm.querySelector('.form-group-related');
+		commentFormErrorEl = commentForm.querySelector('.error-message');
+  };
+
+  var addListeners = function () {
+		addDraftButtonListeners();
+  };
+
+  var addDraftButtonListeners = function () {
+		var buttons = document.querySelectorAll('.js-button-draft');
+		for ( var i = 0; i < buttons.length; i++ ) {
+			buttons[i].addEventListener('click', draftButtonHandler);
+		}
+  };
+
+  var draftButtonHandler = function (event) {
+		if (isValueEmpty(commentField.value)) {
+			event.preventDefault();
+			addErrorToCommentFormGroup();
+		} else {
+			return true;
+		}
+  };
+
+  var addErrorToCommentFormGroup = function () {
+		commentFormGroup.classList.add(errorClass);
+		commentFormErrorEl.innerHTML = errorMessage;
+		commentFormErrorEl.focus();
+	};
+
+  var clearErrorFromCommentField = function () {
+		commentFormGroup.classList.remove(errorClass);
+		commentFormErrorEl.innerHTML = '';
+	};
+
+	var isValueEmpty = function (value) {
+		return (/^ *$/.test(value));
+	};
+
+  var init = function () {
+  	cacheElements();
+    addListeners();
+  };
+
+  return {
+    init: init
+  };
+
+})();
