b42873a4dbb1f10f4b1e3f6489a194c74036c6e4
alex giamas <agiamas@gmail.com>
2017-08-04 13:59:00 +0100

    (fix) multiple attachments, using carrierwave

--- app/models/enquiry_response.rb
+++ app/models/enquiry_response.rb
@@ -1,4 +1,23 @@
 class EnquiryResponse < ActiveRecord::Base
+  mount_uploaders :attachments, EnquiryResponseUploader
+
+  #TODO: add validations
+  # validates_attachment_file_name :attachment, matches: [/ppt\Z/, /pptx\Z/, /pdf\Z/, /doc\Z/, /docx\Z/, /xls\Z/, /xlsx\Z/, /txt\Z/]
+  # validates_attachment_size :attachment, in: 10.bytes..25.megabytes
+  validates :email_body, length: { minimum: 30, message: 'You need at least 30 characters in your reply' }
+  #TODO: scan for viruses
+  # before_save :scan_attachment
+
   belongs_to :enquiry
   belongs_to :editor
+
+  def scan_attachment
+    attachment = attachments.queued_for_write[:original]
+    if attachment
+      ApplicationController.helpers.scan_clean?(attachments.path)
+    else
+      errors.add(:virus_scanner, 'Your attachment is INFECTED. Please contact Export Opportunities helpdesk immediately') unless is_file_clean_or_no_file
+    end
+  end
 end
+
