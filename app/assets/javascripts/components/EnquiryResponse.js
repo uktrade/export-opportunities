cd6756b81475434ec8c6c566311c2ea0cdc9d664
Dermot Byrne <dermotbyrne@dermot.local>
2017-08-07 13:32:44 +0100

    (feature) adding frontend stuff

--- app/assets/javascripts/components/EnquiryResponse.js
+++ app/assets/javascripts/components/EnquiryResponse.js
@@ -5,6 +5,16 @@ ukti.enquiryResponse = (function($) {
   'use strict';
 
   var initTextEditor = function () {
+    CKEDITOR.on('instanceReady', function(evt) {
+      var editor = evt.editor;
+      console.log('The editor named ' + editor.name + ' is now ready');
+      editor.on('focus', function(e) {
+        console.log('The editor named ' + e.editor.name + ' is now focused');
+      });
+      editor.on('blur', function(e) {
+        console.log('The editor named ' + e.editor.name + ' is now blurred');
+      });
+    });
     CKEDITOR.replace( 'enquiry_response_email_body');
   };
 
