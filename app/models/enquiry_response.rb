94a907d4b675df7281ba84ac3f8139dd5af995e1
alex giamas <agiamas@gmail.com>
2017-08-14 21:40:38 +0100

    (fix) attr_accessor for signature in enquiry response

--- app/models/enquiry_response.rb
+++ app/models/enquiry_response.rb
@@ -1,5 +1,7 @@
 class EnquiryResponse < ActiveRecord::Base
   mount_uploaders :attachments, EnquiryResponseUploader
+  attr_accessor :signature
+  
 
   # TODO: add validations
   # validates_attachment_file_name :attachment, matches: [/ppt\Z/, /pptx\Z/, /pdf\Z/, /doc\Z/, /docx\Z/, /xls\Z/, /xlsx\Z/, /txt\Z/]
