7e9c2ca7c3d96cfae3589ccb26c234b5921c34b3
alex giamas <agiamas@gmail.com>
2017-10-03 12:27:19 +0100

    (fix) minor tests changes

--- app/controllers/users/enquiries_controller.rb
+++ app/controllers/users/enquiries_controller.rb
@@ -1,8 +1,7 @@
 module Users
   class EnquiriesController < BaseController
     def show
-      @enquiry = Enquiry.find(params[:id])
-      # @enquiry = current_user.enquiries.find(params[:id])
+      @enquiry = current_user.enquiries.find(params[:id])
       @enquiry_response = @enquiry.enquiry_response
       @response_type = @enquiry_response ? @enquiry_response.response_type : 0
     end
