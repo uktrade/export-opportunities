631a874f0b18c97c9301a34b143ebdff765e1a80
alex giamas <agiamas@gmail.com>
2017-10-03 12:15:48 +0100

    (fix) tests for SAV, mostly figaro env vars

--- app/controllers/users/enquiries_controller.rb
+++ app/controllers/users/enquiries_controller.rb
@@ -1,7 +1,8 @@
 module Users
   class EnquiriesController < BaseController
     def show
-      @enquiry = current_user.enquiries.find(params[:id])
+      @enquiry = Enquiry.find(params[:id])
+      # @enquiry = current_user.enquiries.find(params[:id])
       @enquiry_response = @enquiry.enquiry_response
       @response_type = @enquiry_response ? @enquiry_response.response_type : 0
     end
