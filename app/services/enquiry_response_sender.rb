183ef573f8e0a8395fcc4d18da72062558a5e350
alex giamas <agiamas@gmail.com>
2017-10-25 13:12:37 +0100

    (fix) from is a verified sender in amazon ses, reply_to is the editor's email for PTU.

--- app/services/enquiry_response_sender.rb
+++ app/services/enquiry_response_sender.rb
@@ -3,25 +3,20 @@ class EnquiryResponseSender
     enquiry_response_type = enquiry_response.response_type
     @enquiry = enquiry
     editor_email = enquiry_response.editor.email
-    no_reply_address = Figaro.env.MAILER_FROM_ADDRESS!
-
-    reply_to_address = if [1, 2].include? enquiry_response_type
-                         editor_email.to_s
-                       else
-                         no_reply_address
-                       end
+    no_reply_address = Figaro.env.MAILER_NOREPLY_ADDRESS!
 
     case enquiry_response_type
     when 1
       EnquiryResponseMailer.right_for_opportunity(enquiry_response, editor_email).deliver_later!
     when 2
-      EnquiryResponseMailer.more_information(enquiry_response, editor_email).deliver_later!
+      # EnquiryResponseMailer.more_information(enquiry_response, editor_email).deliver_later!
+      Rails.logger.warn('dont have more information emails')
     when 3
-      EnquiryResponseMailer.not_right_for_opportunity(enquiry_response, editor_email, reply_to_address).deliver_later!
+      EnquiryResponseMailer.not_right_for_opportunity(enquiry_response, editor_email, no_reply_address).deliver_later!
     when 4
-      EnquiryResponseMailer.not_uk_registered(enquiry_response, editor_email, reply_to_address).deliver_later!
+      EnquiryResponseMailer.not_uk_registered(enquiry_response, editor_email, no_reply_address).deliver_later!
     when 5
-      EnquiryResponseMailer.not_for_third_party(enquiry_response, editor_email, reply_to_address).deliver_later!
+      EnquiryResponseMailer.not_for_third_party(enquiry_response, editor_email, no_reply_address).deliver_later!
     end
   end
 end
