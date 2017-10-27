4be498ce88681167971c6d6d4df6d6c3c9e411d6
Dermot Byrne <appility@users.noreply.github.com>
2017-10-27 11:26:52 +0100

    Feature/1080 textchanges (#81)
    * (feature) changing some verbage
    
    * (feature) changing some more verbage
    
    * (feature) titleize helper was failing when passed integer

--- app/helpers/opportunity_helper.rb
+++ app/helpers/opportunity_helper.rb
@@ -1,4 +1,5 @@
 module OpportunityHelper
+
   def toggle_show_expired_link(label, show_expired:)
     show_expired = nil unless show_expired == true
     link_to label, session[:opportunity_filters].merge(show_expired: show_expired)
@@ -7,8 +8,11 @@ module OpportunityHelper
   def opportunity_status_link(string)
     case string
     when 'publish' then 'Published'
+    when 'trash' then 'Trashed'
+    when 'pending' then 'Pending'
+    when 'draft' then 'Draft'   
     else
-      string.titleize
+      string.to_s.titleize
     end
   end
 
