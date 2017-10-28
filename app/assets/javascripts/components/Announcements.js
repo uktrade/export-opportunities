df1c5b802cccc357a5170da153cca539225cf804
Dermot Byrne <dermotbyrne@dermot.local>
2017-10-28 19:37:05 +0100

    (feature) adding env var to suppress announcement in CI

--- app/assets/javascripts/components/Announcements.js
+++ app/assets/javascripts/components/Announcements.js
@@ -6,31 +6,60 @@ ukti.announcements = (function(A11yDialog, Cookies) {
   'use strict';
 
   var config = {
-    DAYS_TO_EXPIRY: 14,
+    DAYS_TO_EXPIRY: 31,
     COOKIE_NAME : 'UPDATE-APRIL-2017-ACCEPTED' // change this when you want to make a new announcement
   };
 
-  var makeAnnouncement = function () {
-    var el = document.getElementById('my-accessible-dialog');
-    var dialogue = new A11yDialog(el);
-    dialogue.show();
-    setCookie();
+  var el,
+      form,
+      dialogue;
+
+  var cacheElements = function () {
+    el = document.getElementById('announcement-dialog');
+    form = el.querySelector( '.dialogue__form' );
   };
 
-  var userHasReadLatestUpdate = function () {
+  var makeAnnouncement = function () {
+    addListeners();
+    dialogue = new A11yDialog(el);
+    dialogue.show();
+  };
+
+  var addListeners = function() {
+    form.addEventListener('submit', handleFormSubmit);
+  };
+
+  var handleFormSubmit = function (event) {
+    event.preventDefault();
+    var checkbox = el.querySelector( '#announcement-accept' );
+    dialogue.hide();
+    if (checkbox.checked) {
+      setUserHasAcceptedAnnouncement();
+    }
+  };
+
+  var userHasAcceptedAnnouncement = function () {
     return Cookies.get(config.COOKIE_NAME);
   };
 
-  var setCookie = function () {
+  var setUserHasAcceptedAnnouncement = function () {
     Cookies.set(config.COOKIE_NAME, 'true', { expires: config.DAYS_TO_EXPIRY });
   };
 
   var init = function () {
-    if (userHasReadLatestUpdate()) {
+    cacheElements();
+    if (!el) {
+      return;
+    }
+    if (userHasAcceptedAnnouncement()) {
       return;
     }
     makeAnnouncement();
   };
+
+  // page.driver.browser.set_cookie("auth_token=#{user.auth_token}")
+  // suppress announcement flag? or nag?
+  // https://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily
 
   return {
     init: init
