0653410e4119a838387a4a0231620d024554a90e
alex giamas <agiamas@gmail.com>
2017-11-03 15:37:43 +0000

    (fix) changing sidekiq schedule frequency

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Figaro.env.redis_url! }
-  schedule_file = 'config/sidekiq_schedule_temp_disable.yml'
+  schedule_file = 'config/sidekiq_schedule.yml'
 
   if File.exist?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
