a74694b87ec60e45dd23fbc8fb3dd0d72f90503b
alex <agiamas@gmail.com>
2017-12-22 17:12:48 +0000

    Separated header/footer js and jquery from aplication js and moved into head to fix header flash (#85)

--- app/assets/javascripts/header-footer.js
+++ app/assets/javascripts/header-footer.js
@@ -464,12 +464,12 @@ headerFooter.scroll = (new function () {
       Expander.focus.call(EXPANDER);
 
       switch(e.which) {
-        case 37: // Fall through.
+        case 38: // Fall through.
         case 27:
         EXPANDER.close();
         break;
         case 13: // Fall through
-        case 39:
+        case 40:
         if(EXPANDER.state === OPEN) {
           // Move though any detected links.
           Expander.move.call(EXPANDER, e);
