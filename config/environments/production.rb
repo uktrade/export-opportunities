8d1f8401a199297525664750ed8ee969e17b1fe6
alex giamas <agiamas@gmail.com>
2018-01-18 22:55:20 +0000

    (remove) remove uglifier from asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
   config.static_cache_control = 'public, max-age=31536000'
 
