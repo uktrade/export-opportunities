5db22afc8a361e83b5593fbfa233121db0fb0f41
alex giamas <agiamas@gmail.com>
2018-02-15 19:58:17 +0000

    (chore) adding quotes in wildcard assets loading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
   config.action_view.raise_on_missing_translations = true
 
 
