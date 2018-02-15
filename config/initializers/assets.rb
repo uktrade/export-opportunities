52c16147e59407dac621e806f9ea530d8ee2e014
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-02-15 15:11:55 +0000

    Remove unwanted JS files from earlier implementation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -32,9 +32,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( updated/admin.css )
 Rails.application.config.assets.precompile += %w( updated/layouts/help.css )
 
-# International (POC) files beyond this point
-Rails.application.config.assets.precompile += %w( international.css )
-Rails.application.config.assets.precompile += %w( international.js )
 
 # POC files beyond this point
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'poc')
@@ -42,6 +39,8 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'image
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'javascripts', 'poc')
 
 Rails.application.config.assets.precompile += %w( poc/global.css )
+Rails.application.config.assets.precompile += %w( poc/ie8_fixes.css )
+Rails.application.config.assets.precompile += %w( poc/ie9_fixes.css )
 Rails.application.config.assets.precompile += %w( poc/layouts/*.css )
 
 Rails.application.config.assets.precompile += %w( poc/*.js )
