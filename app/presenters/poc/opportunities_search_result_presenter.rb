43f5b7245499fb675d8fa33e0c997516382819be
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-03-01 18:37:12 +0000

    Appease rubocop

--- app/presenters/poc/opportunities_search_result_presenter.rb
+++ app/presenters/poc/opportunities_search_result_presenter.rb
@@ -12,18 +12,18 @@ class Poc::OpportunitiesSearchResultPresenter < BasePresenter
   # TODO: What is the view all URL?
   def view_all_link(css_classes = '')
     if @total > @view_limit
-      h.link_to "View all (#{@total})", h.poc_opportunities_path, :class => css_classes
+      h.link_to "View all (#{@total})", h.poc_opportunities_path, 'class': css_classes
     end
   end
 
   def message(css_classes = '')
-    h.content_tag(:p, :class => css_classes) do
+    h.content_tag(:p, 'class': css_classes) do
       h.page_entries_info @results, entry_name: 'item'
     end
   end
 
   def navigation(css_classes = '')
-    h.content_tag(:div, :class => css_classes) do
+    h.content_tag(:div, 'class': css_classes) do
       h.paginate @results, views_prefix: 'poc/components'
     end
   end
