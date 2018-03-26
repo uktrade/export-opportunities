a6bb922331043e5500cbab168c466b46a13c661d
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-03-26 14:17:49 +0100

    Fix incorrectly loading tabs based on calculated position instead of response type

--- app/assets/javascripts/components/EnquiryResponse.js
+++ app/assets/javascripts/components/EnquiryResponse.js
@@ -23,24 +23,25 @@ ukti.EnquiryResponse = (function($) {
       return ukti.Utilities.getValueFromRadioButton('enquiry_response[response_type]');
     };
 
+    // Pass in the current response type, not the tab index number.
     var setState = function (mode) {
       mode = parseInt(mode, 10);
       switch (mode) {
-          case 2:
+          case 3:
               document.getElementById('custom-response').classList.remove('hidden');
               document.getElementById('signature').classList.add('hidden');
               document.getElementById('attachments').classList.add('hidden');
               document.getElementById('submit').classList.remove('hidden');
               updateSubmitButton('Preview');
               break;
-          case 3:
+          case 4:
               document.getElementById('custom-response').classList.add('hidden');
               document.getElementById('signature').classList.add('hidden');
               document.getElementById('attachments').classList.add('hidden');
               document.getElementById('submit').classList.remove('hidden');
               updateSubmitButton('Send');
               break;
-          case 4:
+          case 5:
               document.getElementById('custom-response').classList.add('hidden');
               document.getElementById('signature').classList.add('hidden');
               document.getElementById('attachments').classList.add('hidden');
@@ -56,8 +57,13 @@ ukti.EnquiryResponse = (function($) {
       }
     };
 
-    var tabsCallback = function (index) {
-      setState(index+1);
+    // @index (Number) Clicked tab in order of display.
+    // @el (HTML Node) Actual element to fire click event.
+    var tabsCallback = function (index, el) {
+      var input = document.getElementById(el.getAttribute('for'));
+      if(input) {
+        setState(input.value);
+      }
       ukti.Forms.clearErrorsFromFields();
     };
 
