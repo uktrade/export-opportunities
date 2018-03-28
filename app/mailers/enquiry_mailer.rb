b02982b54fdbc8b12628356c8539a5dbf9965821
stevenburnell-dit <steve.burnell@digital.trade.gov.uk>
2018-03-28 10:33:02 +0100

    Feature/1337 new email notification template post mandatory ptu (#122)
    * (add) new templates for exceptional service providers in post PTU era.
    
    * Update email template to latest design
    
    * (fix) multiple service providers in PTU_EXEMPT_SERVICE_PROVIDERS env var. added new tests
    
    * Remove links to leave only Button as directing source

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -5,12 +5,23 @@ class EnquiryMailer < ApplicationMailer
     @enquiry = enquiry
     return if @enquiry.opportunity.contacts.nil?
 
+    excepted_service_providers = Figaro.env.PTU_EXEMPT_SERVICE_PROVIDERS!
+
     email_addresses = @enquiry.opportunity.contacts.pluck(:email)
 
-    args = {
-      to: email_addresses,
-      subject: 'You’ve received an enquiry: Action required within 5 working days',
-    }
+    args = if (excepted_service_providers.split(',').map(&:to_i).include? @enquiry.opportunity.author.service_provider.id)
+             {
+               template_name: 'send_enquiry_seller_details',
+               to: email_addresses,
+               subject: 'You’ve received an enquiry: Action required within 5 working days',
+             }
+           else
+             {
+               template_name: 'send_enquiry',
+               to: email_addresses,
+               subject: "Enquiry from #{@enquiry.company_name}: action required within 5 working days",
+             }
+           end
 
     if Figaro.env.enquiries_cc_email.present?
       args[:cc] = Figaro.env.enquiries_cc_email
