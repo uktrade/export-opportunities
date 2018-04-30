646e774e6aa59d0d4aa1f8368761f2de10079606
alex giamas <agiamas@gmail.com>
2018-05-01 00:01:01 +0100

    (remove) dead code

--- app/controllers/email_notifications_controller.rb
+++ app/controllers/email_notifications_controller.rb
@@ -1,7 +1,6 @@
 class EmailNotificationsController < ApplicationController
   def show
     user_id = EncryptedParams.decrypt(params[:user_id])
-    date = params[:date]
     today_date = Time.zone.now.strftime('%Y-%m-%d')
 
     @subscription_notifications = SubscriptionNotification.joins(:subscription).where('subscription_notifications.created_at >= ?', today_date).where(sent: true).where('subscriptions.user_id = ?', user_id)
