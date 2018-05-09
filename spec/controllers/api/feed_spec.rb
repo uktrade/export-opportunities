d25159cac9cbb74f0f0bcf7feb544a2b33e04f47
Michal Charemza <michal@charemza.name>
2018-05-09 11:49:37 +0100

    (refactor) Assign to variable to be able to have more specific expectations

--- spec/controllers/api/feed_spec.rb
+++ spec/controllers/api/feed_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Api::FeedController, type: :controller do
     it 'responds with an Atom feed' do
       get :index, params: { format: :xml }
 
-      expect(Hash.from_xml(response.body)).to eq('feed' => {'xmlns' => 'http://www.w3.org/2005/Atom'})
+      xml_hash = Hash.from_xml(response.body)
+      expect(xml_hash).to eq('feed' => {'xmlns' => 'http://www.w3.org/2005/Atom'})
       expect(response.headers['Content-Type']).to eq('application/atom+xml')
     end
   end
