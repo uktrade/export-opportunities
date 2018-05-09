19c176ab41dbffb4c5c40696ea67518b169c2239
Michal Charemza <michal@charemza.name>
2018-05-09 11:46:31 +0100

    (refactor) Separate to multiple strings ready to add more elements

--- app/controllers/api/feed_controller.rb
+++ app/controllers/api/feed_controller.rb
@@ -1,7 +1,10 @@
 module Api
   class FeedController < ApplicationController
     def index
-      contents = '<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://www.w3.org/2005/Atom"></feed>'
+      contents = \
+        '<?xml version="1.0" encoding="UTF-8"?>' \
+        '<feed xmlns="http://www.w3.org/2005/Atom">' \
+        '</feed>'
       respond_to do |format|
         response.headers['Content-Type'] = 'application/atom+xml'
         format.xml { render status: 200, xml: contents}
