a83404fb6cd8a72d5baf1903d4882b8ebf773e4e
Michal Charemza <michal@charemza.name>
2018-05-09 12:20:48 +0100

    (feat) Add id to Atom feed. It is now a valid feed

--- spec/controllers/api/feed_spec.rb
+++ spec/controllers/api/feed_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::FeedController, type: :controller do
   describe 'GET feed controller' do
-    it 'responds with an Atom feed' do
+    it 'responds with an Atom feed with the minimum required elements' do
       get :index, params: { format: :xml }
 
       xml_hash = Hash.from_xml(response.body)
@@ -12,6 +12,8 @@ RSpec.describe Api::FeedController, type: :controller do
 
       rfc3339_pattern = /^(?<fullyear>\d{4})-(?<month>0[1-9]|1[0-2])-(?<mday>0[1-9]|[12][0-9]|3[01])T(?<hour>[01][0-9]|2[0-3]):(?<minute>[0-5][0-9]):(?<second>[0-5][0-9]|60)(?<secfrac>\.[0-9]+)?(Z|(\+|-)(?<offset_hour>[01][0-9]|2[0-3]):(?<offset_minute>[0-5][0-9]))$/i
       expect(feed['updated']).to match(rfc3339_pattern)
+
+      expect(feed['id']).to match(Rails.env)
 
       expect(response.headers['Content-Type']).to eq('application/atom+xml')
     end
