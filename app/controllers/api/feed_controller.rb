5fe11a6eb6cdd0ac52ab365411bf556df2c79874
Michal Charemza <michal@charemza.name>
2018-05-09 12:09:00 +0100

    (feat) Add updated datetime to the feed
    The regex for the test was taken from
    http://mattallan.org/posts/rfc3339-date-time-validation/ . It might let some
    things through that are invalid, but we would really have to try hard to do
    that, so I think it's good enough.

--- app/controllers/api/feed_controller.rb
+++ app/controllers/api/feed_controller.rb
@@ -4,6 +4,7 @@ module Api
       contents = \
         '<?xml version="1.0" encoding="UTF-8"?>' \
         '<feed xmlns="http://www.w3.org/2005/Atom">' \
+          '<updated>' + DateTime.now.to_datetime.rfc3339 + '</updated>' \
           '<title>Export Opportunities Activity Stream</title>' \
         '</feed>'
       respond_to do |format|
