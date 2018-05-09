968cbde719ae48b2bc69700fbf6a953c43a467d4
Michal Charemza <michal@charemza.name>
2018-05-09 16:21:49 +0100

    (feat) Show 403 if the feed shared secret isn't set
    A paranoia check to make sure that we don't accidentally open up the feed in
    case configuration hasn't been done properly. Existing tests do fail if it's
    not set, since a 403 will be returned.

--- app/controllers/api/feed_controller.rb
+++ app/controllers/api/feed_controller.rb
@@ -1,6 +1,8 @@
 module Api
   class FeedController < ApplicationController
     def index
+      return forbidden! if Figaro.env.api_feed_shared_secret.nil? || Figaro.env.api_feed_shared_secret.empty?
+
       contents = \
         '<?xml version="1.0" encoding="UTF-8"?>' \
         '<feed xmlns="http://www.w3.org/2005/Atom">' \
@@ -12,6 +14,10 @@ module Api
         response.headers['Content-Type'] = 'application/atom+xml'
         format.xml { render status: 200, xml: contents}
       end
+    end
+
+    def forbidden!
+      render status: 403, xml: { status: 'Forbidden', code: 403 }.freeze
     end
   end
 end
