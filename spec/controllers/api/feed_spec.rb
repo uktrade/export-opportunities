6fe6d51af1fdfb6a3592ac0ebc897ea9db38a6c7
Michal Charemza <michal@charemza.name>
2018-05-10 11:05:27 +0100

    (refactor) Factor out date pattern so it can be reused

--- spec/controllers/api/feed_spec.rb
+++ spec/controllers/api/feed_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+rfc3339_pattern = /^(?<fullyear>\d{4})-(?<month>0[1-9]|1[0-2])-(?<mday>0[1-9]|[12][0-9]|3[01])T(?<hour>[01][0-9]|2[0-3]):(?<minute>[0-5][0-9]):(?<second>[0-5][0-9]|60)(?<secfrac>\.[0-9]+)?(Z|(\+|-)(?<offset_hour>[01][0-9]|2[0-3]):(?<offset_minute>[0-5][0-9]))$/i
+
 RSpec.describe Api::FeedController, type: :controller do
   describe 'GET feed controller' do
     it 'responds with a 400 if shared_secret is not passed' do
@@ -19,8 +21,6 @@ RSpec.describe Api::FeedController, type: :controller do
       feed = xml_hash['feed']
       expect(feed['xmlns']).to eq('http://www.w3.org/2005/Atom')
       expect(feed['title']).to match(/\S+/)
-
-      rfc3339_pattern = /^(?<fullyear>\d{4})-(?<month>0[1-9]|1[0-2])-(?<mday>0[1-9]|[12][0-9]|3[01])T(?<hour>[01][0-9]|2[0-3]):(?<minute>[0-5][0-9]):(?<second>[0-5][0-9]|60)(?<secfrac>\.[0-9]+)?(Z|(\+|-)(?<offset_hour>[01][0-9]|2[0-3]):(?<offset_minute>[0-5][0-9]))$/i
       expect(feed['updated']).to match(rfc3339_pattern)
 
       expect(feed['id']).to match(Rails.env)
