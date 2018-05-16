675489632f2f83f92d6a2435c13b3311cd1efb20
Michal Charemza <michal@charemza.name>
2018-05-16 17:13:30 +0100

    (feat) Hawk authentication, without nonce
    The content_type and payload _are_ hashed, even if empty. Although
    
      - they're optional by the (rough) spec of Hawk;
      - the Ruby implementation used here tent/hawk-ruby allows it to be optional;
      - the python implementation kumar303/mohawk allows it to be optional;
    
    we have found that
    
      - the Django REST framework implementation, kumar303/hawkrest, does not allow
        it to be optional.
    
    To keep it simple, flexible, and have exactly one authentication mechanism
    between languages, we make sure that the payload and content-type are hashed,
    even if they're empty strings.

--- spec/controllers/api/activity_stream_spec.rb
+++ spec/controllers/api/activity_stream_spec.rb
@@ -1,4 +1,23 @@
+require 'hawk'
 require 'rails_helper'
+
+def auth_header(ts, key_id, secret_key)
+  credentials = {
+    id: key_id,
+    key: secret_key,
+    algorithm: 'sha256',
+  }
+  return Hawk::Client.build_authorization_header(
+    credentials: credentials,
+    ts: ts,
+    method: 'GET',
+    request_uri: '/api/activity_stream',
+    host: 'test.host',
+    port: '443',
+    content_type: '',
+    payload: '',
+  )
+end
 
 RSpec.describe Api::ActivityStreamController, type: :controller do
   describe 'GET feed controller' do
@@ -8,8 +27,48 @@ RSpec.describe Api::ActivityStreamController, type: :controller do
       expect(response.body).to eq(%({"message":"Authorization header is missing"}))
     end
 
-    it 'responds with a blank JSON object if Authorization header is set' do
-      @request.headers['Authorization'] = ''
+    it 'responds with a 401 if Authorization header is set, but timestamped 61 seconds in the past' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i - 61,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Stale ts"}))
+    end
+
+    it 'responds with a 401 if Authorization header uses incorrect key ID' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID + 'something-incorrect',
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Unidentified id"}))
+    end
+
+    it 'responds with a 401 if Authorization header uses incorrect key' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY + 'something-incorrect',
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(401)
+      expect(response.body).to include("Invalid mac")
+    end
+
+    it 'responds with a blank JSON object if Authorization header is set and correct' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
       get :index, params: { format: :json }
 
       expect(response.body).to eq('{}')
