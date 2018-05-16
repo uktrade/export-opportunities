835ee6abdab3e22633594b7171d531eba070610b
Michal Charemza <michal@charemza.name>
2018-05-16 17:13:30 +0100

    (feat) Hawk authentication, without nonce

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -1,9 +1,12 @@
+require 'hawk'
 require 'json'
 
 module Api
   class ActivityStreamController < ApplicationController
     def index
       # 401 if the server can't authenticate the request
+      # 403 is never sent, since there is is no finer granularity for this endpoint:
+      # the holder of the secret key is allowed to access the data
 
       # Ensure Authorization header is sent
       if !request.headers.key?('Authorization') then
@@ -11,6 +14,31 @@ module Api
           response.headers['Content-Type'] = 'application/json'
           error_object = {
             :message => 'Authorization header is missing'
+          }
+          format.json { render status: 401, json: error_object.to_json}
+        end
+        return
+      end
+
+      # Ensure Authorization header is correct
+      credentials = {
+        :id => Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        :key => Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+        :algorithm => "sha256"
+      }
+      res = Hawk::Server.authenticate(
+        request.headers['Authorization'],
+        :method => request.method,
+        :request_uri => request.original_fullpath,
+        :host => request.host,
+        :port => request.standard_port,
+        :credentials_lookup => lambda { |id| id == credentials[:id] ? credentials : nil },
+      )
+      if res != credentials then
+        respond_to do |format|
+          response.headers['Content-Type'] = 'application/json'
+          error_object = {
+            :message => res.message,
           }
           format.json { render status: 401, json: error_object.to_json}
         end
