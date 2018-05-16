a7b4f74261ec9ca50178905e46b761b5f45eb439
Michal Charemza <michal@charemza.name>
2018-05-16 16:38:51 +0100

    (feat) Enforce that Authentication header must be sent with requests
    At this point there is still no actual authentication done

--- spec/controllers/api/activity_stream_spec.rb
+++ spec/controllers/api/activity_stream_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::ActivityStreamController, type: :controller do
   describe 'GET feed controller' do
-    it 'responds with a blank JSON object' do
+    it 'responds with a 401 error if Authorization header is not set' do
+      get :index, params: { format: :json }
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Authorization header is missing"}))
+    end
+
+    it 'responds with a blank JSON object if Authorization header is set' do
+      @request.headers['Authorization'] = ''
       get :index, params: { format: :json }
 
       expect(response.body).to eq('{}')
