8c86267eb0ba57cd775bb5430c431a6a73f85e50
Michal Charemza <michal@charemza.name>
2018-05-16 20:47:35 +0100

    (feat) Check nonce in Hawk authentication to mitigate risk of replay

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -21,6 +21,18 @@ module Api
       end
 
       # Ensure Authorization header is correct
+      check_and_save_nonce = proc do |nonce|
+        redis = Redis.new(url: Figaro.env.redis_url)
+        key = 'activity-stream-nonce-' + nonce
+        key_used = redis.get(key)
+        if key_used
+          true
+        else
+          redis.set(key, true)
+          redis.expire(key, 120)
+          false
+        end
+      end
       credentials = {
         id: Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
         key: Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
@@ -34,7 +46,8 @@ module Api
         port: '443',
         content_type: request.headers['Content-Type'],
         payload: request.body.read,
-        credentials_lookup: ->(id) { id == credentials[:id] ? credentials : nil }
+        credentials_lookup: ->(id) { id == credentials[:id] ? credentials : nil },
+        nonce_lookup: check_and_save_nonce
       )
       if res != credentials
         respond_to do |format|
