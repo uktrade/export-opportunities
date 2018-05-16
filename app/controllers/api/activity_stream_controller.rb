1970c0fe3b965eca84dc155219992b977c9711f7
Michal Charemza <michal@charemza.name>
2018-05-16 16:38:51 +0100

    (feat) Enforce that Authentication header must be sent with requests
    At this point there is still no actual authentication done

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -3,6 +3,20 @@ require 'json'
 module Api
   class ActivityStreamController < ApplicationController
     def index
+      # 401 if the server can't authenticate the request
+
+      # Ensure Authorization header is sent
+      if !request.headers.key?('Authorization') then
+        respond_to do |format|
+          response.headers['Content-Type'] = 'application/json'
+          error_object = {
+            :message => 'Authorization header is missing'
+          }
+          format.json { render status: 401, json: error_object.to_json}
+        end
+        return
+      end
+
       contents = {}
       respond_to do |format|
         response.headers['Content-Type'] = 'application/activity+json'
