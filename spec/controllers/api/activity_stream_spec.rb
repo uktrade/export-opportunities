f543e21e9556e8736b2e84df551c328b60ffd663
Michal Charemza <michal@charemza.name>
2018-05-16 20:47:35 +0100

    (feat) Check nonce in Hawk authentication to mitigate risk of replay

--- spec/controllers/api/activity_stream_spec.rb
+++ spec/controllers/api/activity_stream_spec.rb
@@ -1,5 +1,6 @@
 require 'hawk'
 require 'rails_helper'
+require 'socket'
 
 def auth_header(ts, key_id, secret_key)
   credentials = {
@@ -61,6 +62,40 @@ RSpec.describe Api::ActivityStreamController, type: :controller do
 
       expect(response.status).to eq(401)
       expect(response.body).to include("Invalid mac")
+    end
+
+    it 'responds with a 401 if header is reused' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(200)
+
+      get :index, params: { format: :json }
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Invalid nonce"}))
+    end
+
+    it 'bubbles an error if Authorization header is correct, but Redis is down' do
+      # This test deliberately does not mock the Redis class to raise an Error.
+      # This might be brittle with respect to internals of the Redis class, but
+      # this is chosen as better than assuming that Redis behaves a certain way
+      # in the case of socket errors, since, within reason, it's better for the
+      # test to fail too much than too little.
+      allow_any_instance_of(Socket).to receive(:connect_nonblock).and_raise(SocketError)
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      begin
+        get :index, params: { format: :json }
+      rescue SocketError => ex
+      end
+      expect(ex.backtrace.to_s).to include('/redis/')
     end
 
     it 'responds with a blank JSON object if Authorization header is set and correct' do
