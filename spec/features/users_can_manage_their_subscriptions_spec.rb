a49b25b3d33575ad3516fdce7e5fdbde470cb191
alex giamas <agiamas@gmail.com>
2018-05-30 16:34:22 +0100

    (add) update method for unsubscribe from all notifications to mass update them with the reason

--- spec/features/users_can_manage_their_subscriptions_spec.rb
+++ spec/features/users_can_manage_their_subscriptions_spec.rb
@@ -9,13 +9,12 @@ feature 'User can manage their subscriptions' do
 
     opportunity = create(:opportunity, title: 'Great Opportunity!', slug: 'great-opportunity')
     subscription = create(:subscription, user: user, confirmation_token: token)
-    subscription_notification = create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
+    create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
 
     login_as(user, scope: :user)
 
     visit '/email_notifications/' + user_id
     expect(page.body).to include(opportunity.title)
-    expect(page.body).to include(subscription.search_term)
   end
 
   scenario 'delete all subscriptions' do
@@ -33,6 +32,27 @@ feature 'User can manage their subscriptions' do
     visit '/email_notifications/unsubscribe_all/' + user_id
 
     expect(page.body).to include('deleted')
+  end
+
+  scenario 'delete all subscriptions and provide a reason for doing so' do
+    token = SecureRandom.uuid
+
+    user = create(:user, email: 'john@green.com')
+    user_id = EncryptedParams.encrypt(user.id)
+
+    opportunity = create(:opportunity, title: 'Great Opportunity!', slug: 'great-opportunity')
+    subscription = create(:subscription, user: user, confirmation_token: token)
+    subscription_notification = create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
+
+    login_as(user, scope: :user)
+
+    visit '/email_notifications/unsubscribe_all/' + user_id
+
+    expect(page.body).to include('deleted')
+
+    # select reason
+    # click on submit
+    # expect new page to have some content that we know should only be there if request is successful
   end
 
   scenario 'view a list of all subscription notifications in opportunities filter view' do
