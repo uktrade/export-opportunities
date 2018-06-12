3a5d223b1287a6875a62bf51a93a977af6c04175
alex giamas <agiamas@gmail.com>
2018-06-12 15:27:39 +0100

    (fix) more tests on landing page flow to search

--- spec/features/users_can_view_opportunities_spec.rb
+++ spec/features/users_can_view_opportunities_spec.rb
@@ -136,4 +136,176 @@ RSpec.feature 'User can view opportunities in list', :elasticsearch, :commit do
     expect(page).to_not have_content('tea')
     expect(page).to_not have_content('Japan')
   end
+
+  scenario 'complex scenario of filtering on opportunities. Testing regions, countries and new search in page', :elasticsearch, :commit do
+    # start with 2 opportunities in Mediterranean (1 from Greece, 1 from Italy) and 1 in North East Asia(Japan)
+    mediterranean_country = create(:country, name: 'Greece', slug: 'greece')
+    create(:opportunity, :published, title: 'British pies', countries: [mediterranean_country])
+    another_mediterranean_country = create(:country, name: 'Italy', slug: 'italy')
+    create(:opportunity, :published, title: 'Olive oil', countries: [another_mediterranean_country])
+    asian_country = create(:country, name: 'Japan', slug: 'japan')
+    create(:opportunity, :published, title: 'British tea', countries: [asian_country])
+
+    # just get all opportunities
+    visit '/'
+
+    sleep 1
+    within first('.search-form') do
+      click_on 'Find opportunities'
+    end
+
+    expect(page).to have_content('pies')
+    expect(page).to have_content('oil')
+    expect(page).to have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to have_content('Italy')
+    expect(page).to have_content('Japan')
+
+    expect(page).to_not have_content('Spain')
+    expect(page).to_not have_content('Kenya')
+
+    # select greece and italy from countries
+    find(:css, '#countries_0').set(true)
+    find(:css, '#countries_1').set(true)
+    click_on 'Update results'
+
+    # only Greek and Italian opportunities should be visible
+    expect(page).to have_content('pies')
+    expect(page).to have_content('oil')
+    expect(page).to_not have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to have_content('Italy')
+    expect(page).to_not have_content('Japan')
+
+    expect(page).to have_content('2 results found in Greece or Italy')
+
+    # only select Greece from countries
+    find(:css, '#countries_1').set(false)
+    click_on 'Update results'
+
+    # only Greek opportunity should be visible
+    expect(page).to have_content('pies')
+    expect(page).to_not have_content('oil')
+    expect(page).to_not have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to_not have_content('Italy')
+    expect(page).to_not have_content('Japan')
+
+    expect(page).to have_content('1 result found in Greece')
+
+    # select mediterranean region now
+    find(:css, '#regions_5').set(true)
+    click_on 'Update results'
+
+    # only Greek and Italian(because of Mediterranean Europe) opportunities should be visible
+    expect(page).to have_content('pies')
+    expect(page).to have_content('oil')
+    expect(page).to_not have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to have_content('Italy')
+    expect(page).to_not have_content('Japan')
+
+    expect(page).to have_content('2 results found in Greece or Mediterranean Europe')
+
+    # select North East Asia now
+    # 3 results now. 2 results from before + 1 from North Asia
+    find(:css, '#regions_10').set(true)
+    click_on 'Update results'
+
+    # all opportunities should be visible now
+    expect(page).to have_content('pies')
+    expect(page).to have_content('oil')
+    expect(page).to have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to have_content('Italy')
+    expect(page).to have_content('Japan')
+
+    # shouldn't have other countries in the dropdown list
+    expect(page).to_not have_content('Spain')
+    expect(page).to_not have_content('Canada')
+
+    expect(page).to have_content('3 results found in Greece or Mediterranean Europe or North East Asia')
+
+    # select a region with no results (North America), nothing should change
+    find(:css, '#regions_9').set(true)
+    click_on 'Update results'
+
+    expect(page).to_not have_content('Canada')
+    expect(page).to have_content('3 results found in Greece or Mediterranean Europe or North America or North East Asia')
+
+    # start a new search by searching for oil on the top right hand corner
+    within '.search' do
+      fill_in 's', with: 'oil'
+      find(:css, '.submit').click
+    end
+
+    # only Italian opportunity should be visible, not Greek, not Japanese
+    expect(page).to_not have_content('pies')
+    expect(page).to have_content('oil')
+
+    expect(page).to_not have_content('Greece')
+    expect(page).to have_content('Italy')
+  end
+
+  scenario 'clearing filters after we have some results', :elasticsearch, :commit do
+    # start with 2 opportunities in Mediterranean (1 from Greece, 1 from Italy) and 1 in North East Asia(Japan)
+    mediterranean_country = create(:country, name: 'Greece', slug: 'greece')
+    create(:opportunity, :published, title: 'British pies', countries: [mediterranean_country])
+    another_mediterranean_country = create(:country, name: 'Italy', slug: 'italy')
+    create(:opportunity, :published, title: 'Olive oil', countries: [another_mediterranean_country])
+    asian_country = create(:country, name: 'Japan', slug: 'japan')
+    create(:opportunity, :published, title: 'British tea', countries: [asian_country])
+
+    visit '/'
+
+    sleep 1
+    within first('.search-form') do
+      fill_in 's', with: 'British'
+      click_on 'Find opportunities'
+    end
+
+    # Greek and Japanese opportunities should be visible
+    expect(page).to have_content('pies')
+    expect(page).to_not have_content('oil')
+    expect(page).to have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to_not have_content('Italy')
+    expect(page).to have_content('Japan')
+
+    # Use region filtering to select North East Asia (Japan)
+    find(:css, '#regions_10').set(true)
+    click_on 'Update results'
+
+    # only Japanese opportunity should be visible
+    expect(page).to_not have_content('pies')
+    expect(page).to_not have_content('oil')
+    expect(page).to have_content('tea')
+
+    expect(page).to_not have_content('Greece')
+    expect(page).to_not have_content('Italy')
+    expect(page).to have_content('Japan')
+
+
+    # clear all filters. Can't directly click on the link b/c it has non-ascii character, so we visit the URL with the non-ascii utf8=CHECK_CHARACTER removed
+    clear_all_filters = page.find(:css, 'a[href].reset')[:href]
+    clear_all_filters_link = clear_all_filters.encode('ascii', undef: :replace, replace: '')
+
+    visit clear_all_filters_link
+
+    # Greek and Japanese opportunities should be visible
+    # like they were before filtering
+    expect(page).to have_content('pies')
+    expect(page).to_not have_content('oil')
+    expect(page).to have_content('tea')
+
+    expect(page).to have_content('Greece')
+    expect(page).to_not have_content('Italy')
+    expect(page).to have_content('Japan')
+  end
 end
