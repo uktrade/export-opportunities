60ad7e47009d64b105c6d6e8398a4cee3cc81772
Michal Charemza <michal@charemza.name>
2018-06-12 11:54:02 +0100

    Feature zeus/553 Stub activity stream endpoint (#167)
    * (feat) Endpoint with empty JSON object
    
    No security is needed at this point, since not even dummy secret information is
    revealed.
    
    * (feat) Add Activity Streams 2.0 Content-Type header
    
    * (feat) Enforce that Authentication header must be sent with requests
    
    At this point there is still no actual authentication done
    
    * (feat) Hawk authentication, without nonce
    
    The content_type and payload _are_ hashed, even if empty. Although
    
      - they're optional by the (rough) spec of Hawk;
      - the Ruby implementation used here tent/hawk-ruby allows it to be optional;
      - the python implementation kumar303/mohawk allows it to be optional;
    
    we have found that
    
      - the Django REST framework implementation, kumar303/hawkrest, does not allow
        it to be optional.
    
    To keep it simple, flexible, and have exactly one authentication mechanism
    between languages, we make sure that the payload and content-type are hashed,
    even if they're empty strings.
    
    * (feat) Check nonce in Hawk authentication to mitigate risk of replay
    
    * (feat) Dummy content for pen test to try to expose
    
    * (feat) IP whitelisting on Activity Stream endpoint
    
    The activity stream will be connecting from a short list of IP addresses. This
    is to be able to add a small amount of extra protection over just Hawk.

---
+++ app/controllers/api/activity_stream_controller.rb
@@ -0,0 +1,87 @@
+require 'hawk'
+require 'json'
+
+module Api
+  class ActivityStreamController < ApplicationController
+    def index
+      # 401 if the server can't authenticate the request
+      # 403 is never sent, since there is is no finer granularity for this endpoint:
+      # the holder of the secret key is allowed to access the data
+
+      # Ensure connecting from an authorized IP
+      # request.remote_ip does look at X-Forwarded-For, and so is suitable behind a
+      # load balancer
+      authorized_ip_addresses = Figaro.env.ACTIVITY_STREAM_IP_WHITELIST.split(',')
+      unless authorized_ip_addresses.include?(request.remote_ip)
+        respond_to do |format|
+          response.headers['Content-Type'] = 'application/json'
+          error_object = {
+            message: 'Connecting from unauthorized IP',
+          }
+          format.json { render status: 401, json: error_object.to_json }
+        end
+        return
+      end
+
+      # Ensure Authorization header is sent
+      unless request.headers.key?('Authorization')
+        respond_to do |format|
+          response.headers['Content-Type'] = 'application/json'
+          error_object = {
+            message: 'Authorization header is missing',
+          }
+          format.json { render status: 401, json: error_object.to_json }
+        end
+        return
+      end
+
+      # Ensure Authorization header is correct
+      check_and_save_nonce = proc do |nonce|
+        redis = Redis.new(url: Figaro.env.redis_url)
+        key = 'activity-stream-nonce-' + nonce
+        key_used = redis.get(key)
+        if key_used
+          true
+        else
+          redis.set(key, true)
+          redis.expire(key, 120)
+          false
+        end
+      end
+      credentials = {
+        id: Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        key: Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+        algorithm: 'sha256',
+      }
+      res = Hawk::Server.authenticate(
+        request.headers['Authorization'],
+        method: request.method,
+        request_uri: request.original_fullpath,
+        host: request.host,
+        port: '443',
+        content_type: request.headers['Content-Type'],
+        payload: request.body.read,
+        credentials_lookup: ->(id) { id == credentials[:id] ? credentials : nil },
+        nonce_lookup: check_and_save_nonce
+      )
+      if res != credentials
+        respond_to do |format|
+          response.headers['Content-Type'] = 'application/json'
+          error_object = {
+            'message': res.message,
+          }
+          format.json { render status: 401, json: error_object.to_json }
+        end
+        return
+      end
+
+      contents = {
+        secret: 'content-for-pen-test',
+      }
+      respond_to do |format|
+        response.headers['Content-Type'] = 'application/activity+json'
+        format.json { render status: 200, json: contents.to_json }
+      end
+    end
+  end
+end
