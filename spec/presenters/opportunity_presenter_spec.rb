e9561acbe0c79c6528b031f67e9ae04ff6e61f41
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-06-20 17:42:04 +0100

    Update tests to reflect removal of 'Poc::' presenters

--- spec/presenters/opportunity_presenter_spec.rb
+++ spec/presenters/opportunity_presenter_spec.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 require 'rails_helper'
 
-RSpec.describe Poc::OpportunityPresenter do
+RSpec.describe OpportunityPresenter do
   describe '#initialize' do
     it 'initializes a presenter' do
       opportunity = create(:opportunity, teaser: 'Opportunity teaser')
-      presenter = Poc::OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
+      presenter = OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
 
       expect(presenter.teaser).to eq('Opportunity teaser')
     end
@@ -17,14 +17,14 @@ RSpec.describe Poc::OpportunityPresenter do
       # TODO: opportunity.rb currently stipulates CONTACTS_PER_OPPORTUNITY = 2
       # Believe that validation should change as per Jira[XOT-207]
       opportunity.contacts = []
-      presenter = Poc::OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
+      presenter = OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
 
       expect(presenter.buyer_details_empty?).to eq true
     end
 
     it 'returns false when has buyer name' do
       opportunity = create(:opportunity, buyer_address: 'Buckingham Palace')
-      presenter = Poc::OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
+      presenter = OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
 
       # TODO: Remove contacts reset after Jira[XOT-207] to test this properly.
       opportunity.contacts = []
@@ -34,7 +34,7 @@ RSpec.describe Poc::OpportunityPresenter do
 
     it 'returns false when has buyer address' do
       opportunity = create(:opportunity, buyer_name: 'Fred Spendalot')
-      presenter = Poc::OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
+      presenter = OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
 
       # TODO: Remove contacts reset after Jira[XOT-207] to test this properly.
       opportunity.contacts = []
@@ -44,7 +44,7 @@ RSpec.describe Poc::OpportunityPresenter do
 
     it 'returns false when has a contact' do
       opportunity = create(:opportunity)
-      presenter = Poc::OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
+      presenter = OpportunityPresenter.new(ActionController::Base.helpers, opportunity)
 
       # Expect this to fail when Jira[XOT-207] complete.
       # Will need to create a fake contact(s) after that.
