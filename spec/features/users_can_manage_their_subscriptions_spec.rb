4c68f0c3a38fb61384db4590769a80ed276981b8
alex giamas <agiamas@gmail.com>
2018-06-21 12:33:21 +0100

    (fix) enquiry form prepopulates data protection checkbox

--- spec/features/users_can_manage_their_subscriptions_spec.rb
+++ spec/features/users_can_manage_their_subscriptions_spec.rb
@@ -25,13 +25,13 @@ feature 'User can manage their subscriptions' do
 
     opportunity = create(:opportunity, title: 'Great Opportunity!', slug: 'great-opportunity')
     subscription = create(:subscription, user: user, confirmation_token: token)
-    subscription_notification = create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
+    create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
 
     login_as(user, scope: :user)
 
     visit '/email_notifications/unsubscribe_all/' + user_id
 
-    expect(page.body).to include('deleted')
+    expect(page.body).to include('You have been unsubscribed from all email alerts')
   end
 
   scenario 'delete all subscriptions and provide a reason for doing so' do
@@ -42,7 +42,7 @@ feature 'User can manage their subscriptions' do
 
     opportunity = create(:opportunity, title: 'Great Opportunity!', slug: 'great-opportunity')
     subscription = create(:subscription, user: user, confirmation_token: token)
-    subscription_notification = create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
+    create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
 
     login_as(user, scope: :user)
 
@@ -56,24 +56,6 @@ feature 'User can manage their subscriptions' do
     # get to the next page
     click_on 'submit'
 
-    expect(page.body).to include('Thank you for explaining why you wish to unsubscribe')
-  end
-
-  scenario 'view a list of all subscription notifications in opportunities filter view' do
-    token = SecureRandom.uuid
-
-    user = create(:user, email: 'john@green.com')
-    user_id = EncryptedParams.encrypt(user.id)
-
-    opportunity = create(:opportunity, title: 'Great Opportunity!', slug: 'great-opportunity')
-    subscription = create(:subscription, user: user, confirmation_token: token)
-    subscription_notification = create(:subscription_notification, :sent, subscription: subscription, opportunity: opportunity)
-
-    login_as(user, scope: :user)
-
-    visit '/poc/opportunities/digest/' + user_id
-    
-    expect(page.body).to include(opportunity.title)
-    expect(page.body).to include(subscription.search_term)
+    expect(page.body).to include('Thank you for telling us why you unsubscribed')
   end
 end
