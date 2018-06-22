8ed0f8dff48e8e17f5c966881bfef38d2cbe24c5
Michal Charemza <michal@charemza.name>
2018-06-22 15:23:19 +0100

    Activity stream stub feed (#254)
    * (feat) Endpoint with empty JSON object
    
    No security is needed at this point, since not even dummy secret information is
    revealed.
    
    * (feat) Add Activity Streams 2.0 Content-Type header
    
    * (feat) Enforce that Authentication header must be sent with requests
    
    At this point there is still no actual authentication done
    
    * (feat) Hawk authentication, without nonce
    
    The content_type and payload _are_ hashed, even if empty. Although
    
      - they're optional by the (rough) spec of Hawk;
      - the Ruby implementation used here tent/hawk-ruby allows it to be optional;
      - the python implementation kumar303/mohawk allows it to be optional;
    
    we have found that
    
      - the Django REST framework implementation, kumar303/hawkrest, does not allow
        it to be optional.
    
    To keep it simple, flexible, and have exactly one authentication mechanism
    between languages, we make sure that the payload and content-type are hashed,
    even if they're empty strings.
    
    * (feat) Check nonce in Hawk authentication to mitigate risk of replay
    
    * (feat) Dummy content for pen test to try to expose
    
    * (feat) IP whitelisting on Activity Stream endpoint
    
    The activity stream will be connecting from a short list of IP addresses. This
    is to be able to add a small amount of extra protection over just Hawk.
    
    * (refactor) Reduce duplication
    
    * (refactor) Move nonce check out of index to reduce line length + cog complexity
    
    * (refactor) Move code out of index
    
    This is to move towards the CodeClimate max of 25 lines per method
    
    * (refactor) Move code out of index
    
    This is to move towards the CodeClimate max of 25 lines per method

---
+++ spec/controllers/api/activity_stream_spec.rb
@@ -0,0 +1,118 @@
+require 'hawk'
+require 'rails_helper'
+require 'socket'
+
+def auth_header(ts, key_id, secret_key)
+  credentials = {
+    id: key_id,
+    key: secret_key,
+    algorithm: 'sha256',
+  }
+  return Hawk::Client.build_authorization_header(
+    credentials: credentials,
+    ts: ts,
+    method: 'GET',
+    request_uri: '/api/activity_stream',
+    host: 'test.host',
+    port: '443',
+    content_type: '',
+    payload: '',
+  )
+end
+
+RSpec.describe Api::ActivityStreamController, type: :controller do
+  describe 'GET feed controller' do
+    it 'responds with a 401 error if connecting from unauthorized IP' do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return('1.2.3.4')
+      get :index, params: { format: :json }
+      expect(response.body).to eq(%({"message":"Connecting from unauthorized IP"}))
+    end
+    it 'responds with a 401 error if Authorization header is not set' do
+      get :index, params: { format: :json }
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Authorization header is missing"}))
+    end
+
+    it 'responds with a 401 if Authorization header is set, but timestamped 61 seconds in the past' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i - 61,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Stale ts"}))
+    end
+
+    it 'responds with a 401 if Authorization header uses incorrect key ID' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID + 'something-incorrect',
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Unidentified id"}))
+    end
+
+    it 'responds with a 401 if Authorization header uses incorrect key' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY + 'something-incorrect',
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(401)
+      expect(response.body).to include("Invalid mac")
+    end
+
+    it 'responds with a 401 if header is reused' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.status).to eq(200)
+
+      get :index, params: { format: :json }
+      expect(response.status).to eq(401)
+      expect(response.body).to eq(%({"message":"Invalid nonce"}))
+    end
+
+    it 'bubbles an error if Authorization header is correct, but Redis is down' do
+      # This test deliberately does not mock the Redis class to raise an Error.
+      # This might be brittle with respect to internals of the Redis class, but
+      # this is chosen as better than assuming that Redis behaves a certain way
+      # in the case of socket errors, since, within reason, it's better for the
+      # test to fail too much than too little.
+      allow_any_instance_of(Socket).to receive(:connect_nonblock).and_raise(SocketError)
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      begin
+        get :index, params: { format: :json }
+      rescue SocketError => ex
+      end
+      expect(ex.backtrace.to_s).to include('/redis/')
+    end
+
+    it 'responds with a dummy JSON object if Authorization header is set and correct' do
+      @request.headers['Authorization'] = auth_header(
+        Time.now.getutc.to_i,
+        Figaro.env.ACTIVITY_STREAM_ACCESS_KEY_ID,
+        Figaro.env.ACTIVITY_STREAM_SECRET_ACCESS_KEY,
+      )
+      get :index, params: { format: :json }
+
+      expect(response.body).to eq('{"secret":"content-for-pen-test"}')
+      expect(response.headers['Content-Type']).to eq('application/activity+json')
+    end
+  end
+end
