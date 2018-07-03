728e73de19c7fbdb193341de6c75de95f8657229
alexandros giamas <agiamas@gmail.com>
2018-07-03 13:28:54 +0100

    (remove) invalid test

--- spec/workers/retrieve_volume_opps_spec.rb
+++ spec/workers/retrieve_volume_opps_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RetrieveVolumeOpps, :elasticsearch, :commit, sidekiq: :inline do
   it 'quality checks opps when there are some' do
-    expect(VolumeOppsRetriever).to receive(:call).with(:any).and return({})
+
   end
 
   it 'doesnt do anything when there are no opps to score' do
