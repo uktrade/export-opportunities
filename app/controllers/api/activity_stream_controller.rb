7ddeb8cf3cb0258ce8ec654378d2591f62b80af8
Michal Charemza <michal@charemza.name>
2018-07-23 11:49:11 +0100

    (refactor) Fewer functions, more consistent chain of authentication

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -6,7 +6,12 @@ require 'openssl'
 module Api
   class ActivityStreamController < ApplicationController
     def authenticate(request)
-      return [false, 'Connecting from unauthorized IP'] unless authorized_ip_address?(request)
+      return [false, 'Connecting from unauthorized IP'] unless request.headers.key?('X-Forwarded-For')
+      remote_ips = request.headers['X-Forwarded-For'].split(',')
+      return [false, 'Connecting from unauthorized IP'] unless remote_ips.length >= 2
+      authorized_ip_addresses = Figaro.env.ACTIVITY_STREAM_IP_WHITELIST.split(',')
+      return [false, 'Connecting from unauthorized IP'] unless authorized_ip_addresses.include?(remote_ips[-2])
+
       return [false, 'Authorization header is missing'] unless request.headers.key?('Authorization')
 
       parsed_header_array = request.headers['Authorization'].scan(/([a-z]+)="([^"]+)"/)
@@ -84,17 +89,6 @@ module Api
         response.headers['Content-Type'] = 'application/activity+json'
         format.json { render status: 200, json: contents.to_json }
       end
-    end
-
-    def authorized_ip_address?(request)
-      # Ensure connecting from an authorized IP: the second-to-last IP in
-      # X-Fowarded-For isn't spoofable in PaaS
-
-      return false unless request.headers.key?('X-Forwarded-For')
-
-      remote_ips = request.headers['X-Forwarded-For'].split(',')
-      authorized_ip_addresses = Figaro.env.ACTIVITY_STREAM_IP_WHITELIST.split(',')
-      remote_ips.length >= 2 && authorized_ip_addresses.include?(remote_ips[-2])
     end
 
     def index
