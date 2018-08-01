81f4e73b7de513a15db8d0b5140402893bdc88f7
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-08-01 13:12:50 +0100

    Fix 'undefined method downcase for nil:NilClass' when env.bypass_sso is true

--- spec/support/single_sign_on_helpers.rb
+++ spec/support/single_sign_on_helpers.rb
@@ -1,6 +1,12 @@
 module SingleSignOnHelpers
   def mock_sso_with(email:, uid: '123456')
-    OmniAuth.config.mock_auth[:exporting_is_great] = OmniAuth::AuthHash.new(
+    if Figaro.env.bypass_sso?
+      provider = :developer
+    else
+      provider = :exporting_is_great
+    end
+
+    OmniAuth.config.mock_auth[provider] = OmniAuth::AuthHash.new(
       provider: 'exporting_is_great',
       uid: uid.to_s,
       info: {
