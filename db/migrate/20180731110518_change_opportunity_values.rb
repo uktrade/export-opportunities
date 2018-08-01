32addb2252f0f5831a69f6cb2433402898b8f9e1
alexandros giamas <agiamas@gmail.com>
2018-08-01 09:34:28 +0100

    (fix) consistent slugs in values.

--- db/migrate/20180731110518_change_opportunity_values.rb
+++ db/migrate/20180731110518_change_opportunity_values.rb
@@ -1,20 +1,19 @@
 class ChangeOpportunityValues < ActiveRecord::Migration[5.1]
   def up
-    v1 = Value.find(1)
-    v1.slug = '100k-1m'
+    v1 = Value.where(slug: '100k').first
     v1.name = '£100k-1m'
     v1.save!
-    v2 = Value.find(2)
+    v2 = Value.where(slug: '10k').first
     v2.name = '£0-100k'
     v2.save!
-    v3 = Value.find(3)
+    v3 = Value.where(slug: 'unknown').first
     v3.name = 'unknown'
     v3.save!
-    v4 = Value.new(id:4, slug: '£1m-5m', name: '£1m-5m')
+    v4 = Value.new(id:4, slug: '1m', name: '£1m-5m')
     v4.save!
-    v5 = Value.new(id:5, slug: '£5m-50m', name: '£5m-50m')
+    v5 = Value.new(id:5, slug: '5m', name: '£5m-50m')
     v5.save!
-    v6 = Value.new(id:6, slug: 'more than £50m', name: 'more than £50m')
+    v6 = Value.new(id:6, slug: '500m', name: 'more than £50m')
     v6.save!
 
   end
