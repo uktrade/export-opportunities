b7ca6b8a1b8232e388979f45a2d38d1367ab6257
alex <agiamas@gmail.com>
2018-08-07 12:12:58 +0100

    updating the correct values

--- spec/features/administering_opportunities_spec.rb
+++ spec/features/administering_opportunities_spec.rb
@@ -164,9 +164,9 @@ feature 'Administering opportunities' do
   end
 
   scenario 'Editing an opportunity' do
-    val_1 = create_value(1, '£0-100k')
-    val_2 = create_value(2, 'unknown')
-    val_3 = create_value(3, '£100k-1m')
+    val_1 = create_value(1, '£100k-1m')
+    val_2 = create_value(2, '£0-100k')
+    val_3 = create_value(3, 'unknown')
     admin = create(:admin)
     opportunity = create_opportunity(admin, status: 'pending')
     opportunity.values = [val_2]
