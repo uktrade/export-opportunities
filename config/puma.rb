e799abec58fa772b1334f32cb462fdbd4f3882b8
alexandros giamas <agiamas@gmail.com>
2018-08-17 16:12:47 +0100

    (fix) less threads for puma server, more cookies for domain

--- config/puma.rb
+++ config/puma.rb
@@ -3,9 +3,6 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-#
-# threads_count = ENV.fetch('RAILS_MAX_THREADS') { 16 }
-# threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -61,7 +58,7 @@ daemonize false
 port Integer(ENV.fetch('PORT', 3000))
 environment ENV['RAILS_ENV'] || 'development'
 
-thread_count = Integer(ENV.fetch('THREADS', 32))
+thread_count = Integer(ENV.fetch('THREADS', 30))
 threads thread_count, thread_count
 
 workers Integer(ENV.fetch('PROCESSES', 8))
