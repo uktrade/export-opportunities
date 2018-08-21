df583e78fd518c5e9e02975701e18f0201f6caab
Michal Charemza <michal@charemza.name>
2018-08-21 09:38:03 +0100

    Trivial activity stream (#262)
    * (refactor) Fewer lines of code
    
    * (refactor) More idiomatic
    
    * (refactor) Split authentication to another method
    
    If this isn't done, later changes to index will push cognitive complexity past
    the allowed limit.
    
    * (refactor) Remove unnecessary comments
    
    * (refactor) Signatures more similar
    
    * (refactor) Fewer methods
    
    * (feat) Simple paginated endpoint of ES bulk ingest commands
    
    * (refactor) Rename to be more consistent with activities
    
    * (feat) Closer to Activity Stream format
    
    * (feat) Closer to Activity Stream format
    
    * (feat) Add URL to the enquiry
    
    * (feat) Use a slightly-more-namespaced ID
    
    To avoid clashes, not change in response to URLs changing, and be fairly human
    readable.
    
    * (feat) Give the activity itself a unique ID
    
    * (refactor) Fewer intermediate variables
    
    * (refactor) Fewer functions, more consistent chain of authentication
    
    * (feat) In increasing date order, to better support from beginning of time
    
    * (feat) More deterministic search order
    
    * (refactor) Towards supporting more complex query
    
    * (feat) Always include next href if there are any results, for simplicity
    
    * (feat) More efficient pagination in deep pages
    
    * (feat) More results per page
    
    * (refactor) Remove negation
    
    * (refactor) Remove negation
    
    * (refactor) Reduce cognitive complexity
    
    * (build) Bump limit for number of return statements
    
    This is for activity_stream_controller::authenticate . Trying to wrangle the
    code into fewer return statements would just increase the layer in the code and
    make it harder to follow the logic.
    
    * (fix) Postgres stores timestamps with fractional seconds
    
    Without this, entries were repeated, or even worse, the last page ended up
    giving a link to itself.
    
    * (refactor) Towards changing search_after to consistent timestamp format
    
    * (feat) More consistent timestamp format out of paranoia
    
    * (refactor) Towards multiple error codes
    
    * (feat) Put ActivityStream behind a feature flag
    
    * (feat) Control Flipper activity stream setting from environment
    
    * (feat) Add index to created_at,id for Enquiries, for the activity stream
    
    The index needs to be on both due to the sorting on _both_ created_at and id.
    It could still be used on queries on both however, since, as explained at
    https://www.postgresql.org/docs/9.6/static/indexes-multicolumn.html
    
        A multicolumn B-tree index can be used with query conditions that involve
        any subset of the index's columns

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -3,51 +3,95 @@ require 'digest'
 require 'json'
 require 'openssl'
 
+def to_activity_collection(activities)
+  {
+    '@context': [
+      'https://www.w3.org/ns/activitystreams', {
+        'dit': 'https://www.trade.gov.uk/ns/activitystreams/v1',
+      }
+    ],
+    'type': 'Collection',
+    'orderedItems': activities,
+  }
+end
+
+def to_activity(enquiry)
+  obj_id = 'dit:exportOpportunities:Enquiry:' + enquiry.id.to_s
+  activity_id = obj_id + ':Create'
+  {
+    'id': activity_id,
+    'type': 'Create',
+    'published': enquiry.created_at.to_datetime.rfc3339,
+    'dit:application': 'exportOpportunities',
+    'actor': {
+      'type': ['Organization', 'dit:Company'],
+      'dit:companiesHouseNumber': enquiry.company_house_number,
+    },
+    'object': {
+      'type': ['Document', 'dit:exportOpportunities:Enquiry'],
+      'id': obj_id,
+      'url': admin_enquiry_url(enquiry),
+    },
+  }
+end
+
+def to_search_after(enquiry)
+  timestamp_str = format('%.6f', enquiry.created_at.to_datetime.to_f)
+  id_str = enquiry.id.to_s
+  "#{timestamp_str}_#{id_str}"
+end
+
+MAX_PER_PAGE = 500
+
 module Api
   class ActivityStreamController < ApplicationController
-    def authenticate(
-      authorization_header:,
-      method:, request_uri:, host:, port:,
-      content_type:, payload:
-    )
-      parsed_header_array = authorization_header.scan(/([a-z]+)="([^"]+)"/)
+    def authenticate(request)
+      return [false, 'Connecting from unauthorized IP'] unless request.headers.key?('X-Forwarded-For')
+      remote_ips = request.headers['X-Forwarded-For'].split(',')
+      return [false, 'Connecting from unauthorized IP'] unless remote_ips.length >= 2
+      authorized_ip_addresses = Figaro.env.ACTIVITY_STREAM_IP_WHITELIST.split(',')
+      return [false, 'Connecting from unauthorized IP'] unless authorized_ip_addresses.include?(remote_ips[-2])
+
+      return [false, 'Authorization header is missing'] unless request.headers.key?('Authorization')
+
+      parsed_header_array = request.headers['Authorization'].scan(/([a-z]+)="([^"]+)"/)
       parsed_header = parsed_header_array.each_with_object({}) do |key_val, memo|
         memo[key_val[0].to_sym] = key_val[1]
       end
 
-      return { message: 'Invalid header' }  unless /^Hawk (((?<="), )?[a-z]+="[^"]*")*$/.match?(authorization_header)
-      return { message: 'Missing ts' }      unless parsed_header.key? :ts
-      return { message: 'Invalid ts' }      unless /\d+/.match?(parsed_header[:ts])
-      return { message: 'Missing hash' }    unless parsed_header.key? :hash
-      return { message: 'Missing mac' }     unless parsed_header.key? :mac
-      return { message: 'Missing nonce' }   unless parsed_header.key? :nonce
-      return { message: 'Missing id' }      unless parsed_header.key? :id
-      return { message: 'Unidentified id' } unless secure_compare(correct_credentials[:id], parsed_header[:id])
+      return [false, 'Invalid header']  unless /^Hawk (((?<="), )?[a-z]+="[^"]*")*$/.match?(request.headers['Authorization'])
+      return [false, 'Missing ts']      unless parsed_header.key? :ts
+      return [false, 'Invalid ts']      unless /\d+/.match?(parsed_header[:ts])
+      return [false, 'Missing hash']    unless parsed_header.key? :hash
+      return [false, 'Missing mac']     unless parsed_header.key? :mac
+      return [false, 'Missing nonce']   unless parsed_header.key? :nonce
+      return [false, 'Missing id']      unless parsed_header.key? :id
+      return [false, 'Unidentified id'] unless secure_compare(correct_credentials[:id], parsed_header[:id])
 
-      canonical_payload = 'hawk.1.payload'  + "\n" +
-                          content_type.to_s + "\n" +
-                          payload.to_s      + "\n"
+      canonical_payload = 'hawk.1.payload'                     + "\n" +
+                          request.headers['Content-Type'].to_s + "\n" +
+                          request.body.read.to_s               + "\n"
       correct_payload_hash = Digest::SHA256.base64digest canonical_payload
 
-      canonical_request = 'hawk.1.header'       + "\n" +
-                          parsed_header[:ts]    + "\n" +
-                          parsed_header[:nonce] + "\n" +
-                          method                + "\n" +
-                          request_uri           + "\n" +
-                          host                  + "\n" +
-                          port                  + "\n" +
-                          correct_payload_hash  + "\n" + "\n"
+      canonical_request = 'hawk.1.header'           + "\n" +
+                          parsed_header[:ts]        + "\n" +
+                          parsed_header[:nonce]     + "\n" +
+                          request.method            + "\n" +
+                          request.original_fullpath + "\n" +
+                          request.host              + "\n" \
+                          '443'                     + "\n" +
+                          correct_payload_hash      + "\n" + "\n"
       correct_mac = Base64.encode64(
         OpenSSL::HMAC.digest(
           OpenSSL::Digest.new('sha256'), correct_credentials[:key], canonical_request
         )
       ).strip
-      return { message: 'Invalid hash' }  unless secure_compare(correct_payload_hash, parsed_header[:hash])
-      return { message: 'Stale ts' }      unless (Time.now.getutc.to_i - parsed_header[:ts].to_i).abs <= 60
-      return { message: 'Invalid mac' }   unless secure_compare(correct_mac, parsed_header[:mac])
-      return { message: 'Invalid nonce' } unless nonce_available?(parsed_header[:nonce], parsed_header[:id])
+      return [false, 'Invalid hash']  unless secure_compare(correct_payload_hash, parsed_header[:hash])
+      return [false, 'Stale ts']      unless (Time.now.getutc.to_i - parsed_header[:ts].to_i).abs <= 60
+      return [false, 'Invalid mac']   unless secure_compare(correct_mac, parsed_header[:mac])
+      return [false, 'Invalid nonce'] unless nonce_available?(parsed_header[:nonce], parsed_header[:id])
 
-      correct_credentials
+      [true, '']
     end
 
     def secure_compare(a, b)
@@ -70,13 +114,13 @@ module Api
       }
     end
 
-    def respond_401(message)
+    def respond(code, message)
       respond_to do |format|
         response.headers['Content-Type'] = 'application/json'
         error_object = {
           message: message,
         }
-        format.json { render status: 401, json: error_object.to_json }
+        format.json { render status: code, json: error_object.to_json }
       end
     end
 
@@ -87,49 +131,36 @@ module Api
       end
     end
 
-    def authorized_ip_address?(request)
-      # Ensure connecting from an authorized IP: the second-to-last IP in
-      # X-Fowarded-For isn't spoofable in PaaS
-
-      return false unless request.headers.key?('X-Forwarded-For')
-
-      remote_ips = request.headers['X-Forwarded-For'].split(',')
-      authorized_ip_addresses = Figaro.env.ACTIVITY_STREAM_IP_WHITELIST.split(',')
-      remote_ips.length >= 2 && authorized_ip_addresses.include?(remote_ips[-2])
-    end
-
     def index
       # 401 if the server can't authenticate the request
-      # 403 is never sent, since there is is no finer granularity for this endpoint:
-      # the holder of the secret key is allowed to access the data
+      # 403 is only sent if the activity stream is disabled, since there is
+      # no finer granularity for this endpoint: the holder of the secret key
+      # is allowed to access the data
 
-      unless authorized_ip_address?(request)
-        respond_401 'Connecting from unauthorized IP'
-        return
-      end
+      return respond(403, 'Activity Stream is disabled') unless ExportOpportunities.flipper.enabled?(:activity_stream)
 
-      # Ensure Authorization header is sent
-      unless request.headers.key?('Authorization')
-        respond_401 'Authorization header is missing'
-        return
-      end
+      is_authentic, message = authenticate(request)
+      return respond(401, message) unless is_authentic
 
-      # Ensure Authorization header is correct
-      res = authenticate(
-        authorization_header: request.headers['Authorization'],
-        method: request.method,
-        request_uri: request.original_fullpath,
-        host: request.host,
-        port: '443',
-        content_type: request.headers['Content-Type'],
-        payload: request.body.read
+      search_after = params.fetch(:search_after, '0.000000_0')
+      search_after_time_str, search_after_id_str = search_after.split('_')
+      search_after_time = Float(search_after_time_str)
+      search_after_id = Integer(search_after_id_str)
+      companies_with_number = Enquiry
+        .where("company_house_number IS NOT NULL AND company_house_number != ''")
+        .where('created_at > to_timestamp(?) OR (created_at = to_timestamp(?) AND id > ?)', search_after_time, search_after_time, search_after_id)
+        .order('created_at ASC, id ASC')
+      enquiries = companies_with_number.take(MAX_PER_PAGE)
+
+      items = enquiries.map(&method(:to_activity))
+      contents = to_activity_collection(items).merge(
+        if enquiries.empty?
+          {}
+        else
+          { next: "#{request.base_url}#{request.env['PATH_INFO']}?search_after=#{to_search_after(enquiries[-1])}" }
+        end
       )
-      if res != correct_credentials
-        respond_401 res[:message]
-        return
-      end
-
-      respond_200 secret: 'content-for-pen-test'
+      respond_200 contents
     end
   end
 end
