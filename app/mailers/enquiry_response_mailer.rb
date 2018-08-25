476dc9c4500aa0b5d990da6cf389d4920cb94cb0
Michal Charemza <michal@charemza.name>
2018-08-25 10:49:21 +0100

    (feat) Reduce Figaro usage to reduce memory usage in mailers

--- app/mailers/enquiry_response_mailer.rb
+++ app/mailers/enquiry_response_mailer.rb
@@ -4,13 +4,13 @@ class EnquiryResponseMailer < ApplicationMailer
   def right_for_opportunity(enquiry_response, editor_email)
     @mailer_view = true
     @enquiry_response = enquiry_response
-    mail from: Figaro.env.MAILER_FROM_ADDRESS!, name: 'Export opportunities', reply_to: editor_email, to: enquiry_response.enquiry.user.email, bcc: editor_email, subject: "Update on your enquiry for the export opportunity #{enquiry_response.enquiry.opportunity.title}"
+    mail from: ENV.fetch('MAILER_FROM_ADDRESS'), name: 'Export opportunities', reply_to: editor_email, to: enquiry_response.enquiry.user.email, bcc: editor_email, subject: "Update on your enquiry for the export opportunity #{enquiry_response.enquiry.opportunity.title}"
   end
 
   def more_information(enquiry_response, editor_email)
     @mailer_view = true
     @enquiry_response = enquiry_response
-    mail from: Figaro.env.MAILER_FROM_ADDRESS!, name: 'Export opportunities', reply_to: editor_email, to: enquiry_response.enquiry.user.email, bcc: editor_email, subject: "Update on your enquiry for the export opportunity #{enquiry_response.enquiry.opportunity.title}"
+    mail from: ENV.fetch('MAILER_FROM_ADDRESS'), name: 'Export opportunities', reply_to: editor_email, to: enquiry_response.enquiry.user.email, bcc: editor_email, subject: "Update on your enquiry for the export opportunity #{enquiry_response.enquiry.opportunity.title}"
   end
 
   def not_right_for_opportunity(enquiry_response, editor_email, reply_to_address)
