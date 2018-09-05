5cc78516114b478f55562741b02ba504daddab2b
alexandros giamas <agiamas@gmail.com>
2018-09-05 13:48:20 +0100

    (fix) fetch volume opps from yesterday to include non-TED sources but only run checks in today's fetched opps

--- app/workers/retrieve_volume_opps.rb
+++ app/workers/retrieve_volume_opps.rb
@@ -7,14 +7,15 @@ class RetrieveVolumeOpps
     editor = Editor.where(email: Figaro.env.MAILER_FROM_ADDRESS!).first
 
     # we need to fetch from yesterday to tomorrow to be able to get both TED and other sources that get ingested overnight
-    yesterday_date = (Time.zone.now - 1.day).strftime('%Y-%m-%d')
+    today_date = Time.zone.now.strftime('%Y-%m-%d')
+    yesterday_date = today_date - 1.day
     from_date = yesterday_date
     to_date = (Time.zone.now + 1.day).strftime('%Y-%m-%d')
 
     # fetch opportunities
     VolumeOppsRetriever.new.call(editor, from_date, to_date)
 
-    opportunity_result_set = Opportunity.where('created_at>? and source=1', yesterday_date)
+    opportunity_result_set = Opportunity.where('created_at>? and source=1', today_date)
 
     # run sensitivity checks
     opportunity_result_set.each do |opportunity|
@@ -32,6 +33,6 @@ class RetrieveVolumeOpps
     end
 
     # force re-indexing of results
-    Opportunity.import query: -> { where('created_at>? and source=1', yesterday_date) }
+    Opportunity.import query: -> { where('created_at>? and source=1', today_date) }
   end
 end
