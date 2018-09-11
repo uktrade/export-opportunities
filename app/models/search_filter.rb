7ed85e623ac58da8338e66ad5bb76dbdaf46b431
alexandros giamas <agiamas@gmail.com>
2018-09-11 15:05:06 +0100

    (remove) dead/unused code

--- app/models/search_filter.rb
+++ app/models/search_filter.rb
@@ -1,5 +1,5 @@
 class SearchFilter
-  attr_reader :sectors, :regions, :countries, :types, :values, :publish_date
+  attr_reader :sectors, :regions, :countries, :types, :values
 
   def initialize(params = {})
     @params = params
@@ -24,10 +24,6 @@ class SearchFilter
 
   def values
     @values ||= whitelisted_filters_for(:values, Value)
-  end
-
-  def publish_date
-    @publish_date ||= true
   end
 
   private def whitelisted_filters_for(name, klass)
