50a9c10f606a750b3990788eaaf2a97714ccf4d5
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-09-21 12:32:19 +0100

    Remove A/B testing on Opportunity button

--- app/assets/javascripts/transformation/dit.page.opportunity.js
+++ app/assets/javascripts/transformation/dit.page.opportunity.js
@@ -5,13 +5,8 @@
 
 dit.page.opportunity = (new function () {
 
-  // Set immediately.
-  this.abcButtonText = generateAbcButtonText();
-
-
   // Page init
   this.init = function() {
-    applyAbcButtonTesting();
     clipDescription();
   }
 
@@ -19,33 +14,6 @@ dit.page.opportunity = (new function () {
     new dit.classes.SimpleTextDisplayRestrictor($("dd.description"), 50);
   }
 
-  /* Quick and simple a/b/c testing.
-   * Will create a bigger/more re-usable chunk of 
-   * functionality when have time and/or need.
-   **/
-  function applyAbcButtonTesting() {
-    var $button = $(".bid .abcbutton");
-    var text = dit.page.opportunity.abcButtonText;
-
-    // Update to the required wording. 
-    $button.data("abcbuttontext", text);
-    $button.text(text);
-
-    // Add custom GA event to send value.
-    $button.on("click", function(e) {
-      ga("send", "event", "OpportunityButtonText", "click", dit.page.opportunity.abcButtonText);
-    });
-  }
-
-  /* Generates appropriate a/b/c button text.
-   * Sets value for use in GA
-   * Returns value.
-   **/
-  function generateAbcButtonText() {
-    return ["", // Not required.
-      "Express your interest", 
-      "Register your interest"][Math.floor((Math.random() * 2) + 1)];
-  }
 });
 
 // Init needs to wait for domReady.
