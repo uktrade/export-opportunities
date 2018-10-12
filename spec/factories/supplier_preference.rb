665317a5bae2e4110f52ad2172fa5bfba098aa65
alexandros giamas <agiamas@gmail.com>
2018-10-12 09:27:05 +0100

    (fix) tests mostly fixed

--- spec/factories/supplier_preference.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  super_name = Faker::Superhero.name
-  factory :supplier_preference do
-    name { super_name }
-    slug { super_name.parameterize }
-
-  end
-end
--- spec/factories/types.rb
+++ spec/factories/types.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :type do
-    slug { Faker::Lorem.words(10).join('-') }
+    sequence(:slug) { |n| [name.downcase.parameterize, n].join('-') }
     name { Faker::Superhero.name }
   end
 end
