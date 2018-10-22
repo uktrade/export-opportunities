c1036b0b607969f7b5beebd7b35d8e5d5fd79984
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-10-22 11:11:49 +0100

    Add creation date to the Opportunity Show view

--- app/presenters/opportunity_presenter.rb
+++ app/presenters/opportunity_presenter.rb
@@ -51,6 +51,10 @@ class OpportunityPresenter < BasePresenter
     present_html_or_formatted_text(opportunity.description).html_safe
   end
 
+  def created
+    opportunity.created_at.strftime('%d %B %Y')
+  end
+
   def expires
     opportunity.response_due_on.strftime('%d %B %Y')
   end
