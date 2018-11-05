272de87d9f5476746dba7ad4e08244209c29fcab
stevenburnell-dit <steve.burnell@digital.trade.gov.uk>
2018-11-05 10:14:01 +0000

    Bugfix/XOT-505 edit opps country selection (#389)
    * Fix to prevent default selected country ignoring actual selected countries
    
    * Remove function because order should be a-z and no longer needs selection promotion due to new component

--- app/controllers/admin/opportunities_controller.rb
+++ app/controllers/admin/opportunities_controller.rb
@@ -130,12 +130,13 @@ class Admin::OpportunitiesController < Admin::BaseController
     @tender_bool = %w[true false]
     @request_usages = Opportunity.request_usages.keys
     @supplier_preferences = SupplierPreference.all
-    @countries = promote_elements_to_front_of_array(Country.all.order(:name), opportunity.countries.sort_by(&:name))
-    @sectors = promote_elements_to_front_of_array(Sector.all.order(:name), opportunity.sectors.sort_by(&:name))
-    @types = Type.all.order(:name)
-    @values = Value.all.order(:slug)
     @service_providers = ServiceProvider.all.order(:name)
     @selected_service_provider = opportunity.service_provider || current_editor.service_provider
+    @countries = Country.all.order(:name)
+    @default_country = @selected_service_provider.country&.id if opportunity.countries.empty?
+    @sectors = Sector.all.order(:name)
+    @types = Type.all.order(:name)
+    @values = Value.all.order(:slug)
     @enquiry_interactions = Opportunity.enquiry_interactions.keys
     @ragg = opportunity.ragg
   end
