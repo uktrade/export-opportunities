a3a528ca37344f5771880534844c1207eab8972c
alex <agiamas@gmail.com>
2018-11-05 10:13:10 +0000

    (fix) now sending impact email 3 months after enquiry, instead of 3 months after opp expiry date (#390)

--- spec/services/enquiry_feedback_survey_sender_spec.rb
+++ spec/services/enquiry_feedback_survey_sender_spec.rb
@@ -3,21 +3,25 @@ require 'rails_helper'
 RSpec.describe EnquiryFeedbackSurveySender do
   describe 'start_date and end_date' do
     it 'selects enquiries from a given date range, not inclusive' do
-      yesterday = Time.zone.now.beginning_of_day - 1.day
-      opp = create(:opportunity, response_due_on: yesterday - 3.months)
-      enquiries_in_range = [
-        create(:enquiry, opportunity: opp, created_at: yesterday),
-        create(:enquiry, opportunity: opp, created_at: yesterday + 12.hours),
-        create(:enquiry, opportunity: opp, created_at: yesterday + 1.day),
-      ]
+      today = Time.zone.now.beginning_of_day
+      default_start_date = today - 3.months - 1.day
+      default_end_date = today - 3.months
 
+      opp = create(:opportunity, response_due_on: today)
+      enquiries_in_range = [
+        create(:enquiry, opportunity: opp, created_at: default_start_date),
+        create(:enquiry, opportunity: opp, created_at: default_start_date + 12.hours),
+        create(:enquiry, opportunity: opp, created_at: default_end_date),
+      ]
       enquiries_not_in_range = [
-        create(:enquiry, created_at: yesterday - 1.second),
-        create(:enquiry, created_at: yesterday + 1.day + 1.second),
-        create(:enquiry, created_at: yesterday - 3.months),
+        create(:enquiry, created_at: default_start_date - 1.second),
+        create(:enquiry, created_at: default_end_date + 1.second),
+        create(:enquiry, created_at: default_start_date - 1.day),
+        create(:enquiry, created_at: default_end_date + 1.day),
       ]
 
       returned = EnquiryFeedbackSurveySender.new.call
+
       expect(returned).to include(*enquiries_in_range.map(&:id))
       expect(returned).not_to include(*enquiries_not_in_range.map(&:id))
     end
@@ -39,11 +43,14 @@ RSpec.describe EnquiryFeedbackSurveySender do
   end
 
   it 'dispatches the matched enquiries to EnquiryFeedbackSender' do
-    yesterday = Time.zone.now.beginning_of_day - 1.day
-    opp = create(:opportunity, response_due_on: yesterday - 3.months)
+    today = Time.zone.now.beginning_of_day
+    default_start_date = today - 3.months - 1.day
+    default_end_date = today - 3.months
 
-    first_matched_enquiry = create(:enquiry, created_at: yesterday, opportunity: opp)
-    second_matched_enquiry = create(:enquiry, created_at: yesterday + 1.day, opportunity: opp)
+    opp = create(:opportunity, response_due_on: default_start_date + 1.month)
+
+    first_matched_enquiry = create(:enquiry, created_at: default_start_date, opportunity: opp)
+    second_matched_enquiry = create(:enquiry, created_at: default_end_date, opportunity: opp)
 
     expect_any_instance_of(EnquiryFeedbackSender).to receive(:call).with(first_matched_enquiry.id).once
     expect_any_instance_of(EnquiryFeedbackSender).to receive(:call).with(second_matched_enquiry.id).once
