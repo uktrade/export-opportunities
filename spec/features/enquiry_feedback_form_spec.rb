834f094ec8c970f6aeb2ea7234818898ae453563
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-11-05 13:19:05 +0000

    Fix broken test

--- spec/features/enquiry_feedback_form_spec.rb
+++ spec/features/enquiry_feedback_form_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 feature 'users can submit feedback after impact email has been sent to them' do
   scenario 'a user clicks on the I won this opportunity link in the impact email, lands in our feedback form page and submits feedback' do
+    content = get_content('enquiry_feedback')
     feedback = create(:enquiry_feedback)
     params = { id: feedback.id, response: :won }
-    feedback_message = Faker::Lorem.sentence(10, false, 5)
+    feedback_message = 'some user feedback'
 
     expect(EncryptedParams).to receive(:decrypt).with(nil).and_return(params)
     visit enquiry_feedback_url(params[:id])
     feedback.reload
-    fill_in 'Leave feedback', with: feedback_message
-    click_on 'Submit'
+
+    expect(page).to have_content content['response_breadcrumb']
+    fill_in 'enquiry_feedback[message]', with: feedback_message
+    click_on content['response_submit']
+    byebug
     feedback.reload
 
     expect(feedback).to be_won
