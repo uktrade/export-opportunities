997f3eea9c645df4fb32465815556bef4c56ac57
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-12-11 17:29:24 +0000

    Add tests for retrieving name arrays

--- spec/models/search_filter_spec.rb
+++ spec/models/search_filter_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'rails_helper'
 
 RSpec.describe SearchFilter, type: :service do
@@ -28,6 +29,17 @@ RSpec.describe SearchFilter, type: :service do
       expect(filter.sectors).to include('airports')
       expect(filter.sectors).to include('stations')
     end
+
+    it 'returns an array of sector names' do
+      create(:sector, name: 'Airport sector', slug: 'airports')
+      create(:sector, name: 'Station sector',  slug: 'stations')
+      params = { sectors: %w[airports stations] }
+      filter = SearchFilter.new(params)
+
+      expect(filter.sectors(:name).length).to eq(2)
+      expect(filter.sectors(:name)).to include('Airport sector')
+      expect(filter.sectors(:name)).to include('Station sector')
+    end
   end
 
   describe '.regions' do
@@ -47,14 +59,21 @@ RSpec.describe SearchFilter, type: :service do
     end
 
     it 'returns an array of region slugs' do
-      create(:sector, slug: 'europe')
-      create(:sector, slug: 'asia')
-      params = { sectors: %w[europe asia] }
+      params = { region: %w[europe asia] }
       filter = SearchFilter.new(params)
 
-      expect(filter.sectors.length).to eq(2)
-      expect(filter.sectors).to include('europe')
-      expect(filter.sectors).to include('asia')
+      expect(filter.regions.length).to eq(2)
+      expect(filter.regions).to include('europe')
+      expect(filter.regions).to include('asia')
+    end
+
+    it 'returns an array of region names' do
+      params = { regions: %w[europe asia] }
+      filter = SearchFilter.new(params)
+
+      expect(filter.regions(:name).length).to eq(2)
+      expect(filter.regions(:name)).to include('Europe')
+      expect(filter.regions(:name)).to include('Asia')
     end
   end
 
@@ -76,14 +95,25 @@ RSpec.describe SearchFilter, type: :service do
     end
 
     it 'returns an array of country slugs' do
-      create(:sector, slug: 'france')
-      create(:sector, slug: 'spain')
-      params = { sectors: %w[france spain] }
+      create(:country, slug: 'france')
+      create(:country, slug: 'spain')
+      params = { countries: %w[france spain] }
       filter = SearchFilter.new(params)
 
-      expect(filter.sectors.length).to eq(2)
-      expect(filter.sectors).to include('france')
-      expect(filter.sectors).to include('spain')
+      expect(filter.countries.length).to eq(2)
+      expect(filter.countries).to include('france')
+      expect(filter.countries).to include('spain')
+    end
+
+    it 'returns an array of country names' do
+      create(:country, name: 'France', slug: 'france')
+      create(:country, name: 'Spain',  slug: 'spain')
+      params = { countries: %w[france spain] }
+      filter = SearchFilter.new(params)
+
+      expect(filter.countries(:name).length).to eq(2)
+      expect(filter.countries(:name)).to include('France')
+      expect(filter.countries(:name)).to include('Spain')
     end
   end
 
@@ -105,14 +135,25 @@ RSpec.describe SearchFilter, type: :service do
     end
 
     it 'returns an array of type slugs' do
-      create(:sector, slug: 'public-sector')
-      create(:sector, slug: 'private-sector')
-      params = { sectors: %w[public-sector private-sector] }
+      create(:type, slug: 'public-sector')
+      create(:type, slug: 'private-sector')
+      params = { types: %w[public-sector private-sector] }
       filter = SearchFilter.new(params)
 
-      expect(filter.sectors.length).to eq(2)
-      expect(filter.sectors).to include('public-sector')
-      expect(filter.sectors).to include('private-sector')
+      expect(filter.types.length).to eq(2)
+      expect(filter.types).to include('public-sector')
+      expect(filter.types).to include('private-sector')
+    end
+
+    it 'returns an array of type names' do
+      create(:type, name: 'Public Sector', slug: 'public-sector')
+      create(:type, name: 'Private Sector',  slug: 'private-sector')
+      params = { types: %w[public-sector private-sector] }
+      filter = SearchFilter.new(params)
+
+      expect(filter.types(:name).length).to eq(2)
+      expect(filter.types(:name)).to include('Public Sector')
+      expect(filter.types(:name)).to include('Private Sector')
     end
   end
 
@@ -134,14 +175,25 @@ RSpec.describe SearchFilter, type: :service do
     end
 
     it 'returns an array of value slugs' do
-      create(:sector, slug: 'unknown')
-      create(:sector, slug: 'over-amount')
-      params = { sectors: %w[unknown over-amount] }
+      create(:value, slug: 'unknown')
+      create(:value, slug: 'over-amount')
+      params = { values: %w[unknown over-amount] }
       filter = SearchFilter.new(params)
 
-      expect(filter.sectors.length).to eq(2)
-      expect(filter.sectors).to include('unknown')
-      expect(filter.sectors).to include('over-amount')
+      expect(filter.values.length).to eq(2)
+      expect(filter.values).to include('unknown')
+      expect(filter.values).to include('over-amount')
+    end
+
+    it 'returns an array of value names' do
+      create(:value, name: 'Unknown', slug: 'unknown')
+      create(:value, name: 'over £100K',  slug: 'over-amount')
+      params = { values: %w[unknown over-amount] }
+      filter = SearchFilter.new(params)
+
+      expect(filter.values(:name).length).to eq(2)
+      expect(filter.values(:name)).to include('Unknown')
+      expect(filter.values(:name)).to include('over £100K')
     end
   end
 end
