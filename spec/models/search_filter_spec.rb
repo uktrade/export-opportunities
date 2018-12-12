1e6e836f5d965c77a51e30864f038e00a551c29e
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-12-12 14:53:02 +0000

    Tidy up to avoid repetitive function calls

--- spec/models/search_filter_spec.rb
+++ spec/models/search_filter_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 
 RSpec.describe SearchFilter, type: :service do
   describe '.sectors' do
-    it 'only contains whitelisted sectors' do
+    it 'only contains whitelisted sector slugs' do
       create(:sector, slug: 'airports')
       params = { sectors: %w[mind-control airports] }
       filters = SearchFilter.new(params)
+      sector_slugs = filters.sectors
 
-      expect(filters.sectors).to include('airports')
-      expect(filters.sectors).not_to include('mind-control')
+      expect(sector_slugs).to include('airports')
+      expect(sector_slugs).not_to include('mind-control')
     end
 
     it 'returns an empty array when no sector params' do
@@ -24,10 +25,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:sector, slug: 'stations')
       params = { sectors: %w[airports stations] }
       filter = SearchFilter.new(params)
+      sector_slugs = filter.sectors
 
-      expect(filter.sectors.length).to eq(2)
-      expect(filter.sectors).to include('airports')
-      expect(filter.sectors).to include('stations')
+      expect(sector_slugs.length).to eq(2)
+      expect(sector_slugs).to include('airports')
+      expect(sector_slugs).to include('stations')
     end
 
     it 'returns an array of sector names' do
@@ -35,10 +37,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:sector, name: 'Station sector',  slug: 'stations')
       params = { sectors: %w[airports stations] }
       filter = SearchFilter.new(params)
+      sector_names = filter.sectors(:name)
 
-      expect(filter.sectors(:name).length).to eq(2)
-      expect(filter.sectors(:name)).to include('Airport sector')
-      expect(filter.sectors(:name)).to include('Station sector')
+      expect(sector_names.length).to eq(2)
+      expect(sector_names).to include('Airport sector')
+      expect(sector_names).to include('Station sector')
     end
 
     it 'returns an array of sector data' do
@@ -60,9 +63,10 @@ RSpec.describe SearchFilter, type: :service do
     it 'only contains whitelisted regions' do
       params = { regions: %w[western-europe hackistan] }
       filter = SearchFilter.new(params)
+      region_slugs = filter.regions
 
-      expect(filter.regions).to include('western-europe')
-      expect(filter.regions).not_to include('hackistan')
+      expect(region_slugs).to include('western-europe')
+      expect(region_slugs).not_to include('hackistan')
     end
 
     it 'returns an empty array when no region params' do
@@ -75,19 +79,21 @@ RSpec.describe SearchFilter, type: :service do
     it 'returns an array of region slugs' do
       params = { regions: %w[western-europe south-asia] }
       filter = SearchFilter.new(params)
+      region_slugs = filter.regions
 
-      expect(filter.regions.length).to eq(2)
-      expect(filter.regions).to include('western-europe')
-      expect(filter.regions).to include('south-asia')
+      expect(region_slugs.length).to eq(2)
+      expect(region_slugs).to include('western-europe')
+      expect(region_slugs).to include('south-asia')
     end
 
     it 'returns an array of region names' do
       params = { regions: %w[western-europe south-asia] }
       filter = SearchFilter.new(params)
+      region_names = filter.regions(:name)
 
-      expect(filter.regions(:name).length).to eq(2)
-      expect(filter.regions(:name)).to include('Western Europe')
-      expect(filter.regions(:name)).to include('South Asia')
+      expect(region_names.length).to eq(2)
+      expect(region_names).to include('Western Europe')
+      expect(region_names).to include('South Asia')
     end
 
     it 'returns an array of region data' do
@@ -107,10 +113,11 @@ RSpec.describe SearchFilter, type: :service do
     it 'only contains whitelisted countries' do
       create(:country, slug: 'australia')
       params = { countries: %w[australia hackistan] }
-      filters = SearchFilter.new(params)
+      filter = SearchFilter.new(params)
+      country_slugs = filter.countries
 
-      expect(filters.countries).to include('australia')
-      expect(filters.countries).not_to include('hackistan')
+      expect(country_slugs).to include('australia')
+      expect(country_slugs).not_to include('hackistan')
     end
 
     it 'returns an empty array when no country params' do
@@ -125,10 +132,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:country, slug: 'spain')
       params = { countries: %w[france spain] }
       filter = SearchFilter.new(params)
+      country_slugs = filter.countries
 
-      expect(filter.countries(:slug).length).to eq(2)
-      expect(filter.countries(:slug)).to include('france')
-      expect(filter.countries(:slug)).to include('spain')
+      expect(country_slugs.length).to eq(2)
+      expect(country_slugs).to include('france')
+      expect(country_slugs).to include('spain')
     end
 
     it 'returns an array of country names' do
@@ -136,10 +144,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:country, name: 'Spain',  slug: 'spain')
       params = { countries: %w[france spain] }
       filter = SearchFilter.new(params)
+      country_names = filter.countries(:name)
 
-      expect(filter.countries(:name).length).to eq(2)
-      expect(filter.countries(:name)).to include('France')
-      expect(filter.countries(:name)).to include('Spain')
+      expect(country_names.length).to eq(2)
+      expect(country_names).to include('France')
+      expect(country_names).to include('Spain')
     end
 
     it 'returns an array of country data' do
@@ -164,13 +173,14 @@ RSpec.describe SearchFilter, type: :service do
       create(:country, slug: 'taiwan')
       params = { countries: %w[france spain], regions: %w[north-east-asia] }
       filter = SearchFilter.new(params)
+      country_slugs = filter.countries
 
-      expect(filter.countries.length).to eq(5)
-      expect(filter.countries).to include('france')
-      expect(filter.countries).to include('spain')
-      expect(filter.countries).to include('japan')
-      expect(filter.countries).to include('south-korea')
-      expect(filter.countries).to include('taiwan')
+      expect(country_slugs.length).to eq(5)
+      expect(country_slugs).to include('france')
+      expect(country_slugs).to include('spain')
+      expect(country_slugs).to include('japan')
+      expect(country_slugs).to include('south-korea')
+      expect(country_slugs).to include('taiwan')
     end
   end
 
@@ -178,10 +188,11 @@ RSpec.describe SearchFilter, type: :service do
     it 'only contains whitelisted types' do
       create(:type, slug: 'public')
       params = { types: %w[evil-empire public] }
-      filters = SearchFilter.new(params)
+      filter = SearchFilter.new(params)
+      type_slugs = filter.types
 
-      expect(filters.types).to include('public')
-      expect(filters.types).not_to include('evil-empire')
+      expect(type_slugs).to include('public')
+      expect(type_slugs).not_to include('evil-empire')
     end
 
     it 'returns an empty array when no type params' do
@@ -196,10 +207,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:type, slug: 'private')
       params = { types: %w[public private] }
       filter = SearchFilter.new(params)
+      type_slugs = filter.types
 
-      expect(filter.types.length).to eq(2)
-      expect(filter.types).to include('public')
-      expect(filter.types).to include('private')
+      expect(type_slugs.length).to eq(2)
+      expect(type_slugs).to include('public')
+      expect(type_slugs).to include('private')
     end
 
     it 'returns an array of type names' do
@@ -207,10 +219,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:type, name: 'Private Sector',  slug: 'private')
       params = { types: %w[public private] }
       filter = SearchFilter.new(params)
+      type_names = filter.types(:name)
 
-      expect(filter.types(:name).length).to eq(2)
-      expect(filter.types(:name)).to include('Public Sector')
-      expect(filter.types(:name)).to include('Private Sector')
+      expect(type_names.length).to eq(2)
+      expect(type_names).to include('Public Sector')
+      expect(type_names).to include('Private Sector')
     end
 
     it 'returns an array of type data' do
@@ -232,10 +245,11 @@ RSpec.describe SearchFilter, type: :service do
     it 'only contains whitelisted values' do
       create(:value, slug: 'unknown')
       params = { values: %w[nothing unknown] }
-      filters = SearchFilter.new(params)
+      filter = SearchFilter.new(params)
+      value_slugs = filter.values
 
-      expect(filters.values).to include('unknown')
-      expect(filters.values).not_to include('nothing')
+      expect(value_slugs).to include('unknown')
+      expect(value_slugs).not_to include('nothing')
     end
 
     it 'returns an empty array when no value params' do
@@ -250,10 +264,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:value, slug: 'over-amount')
       params = { values: %w[unknown over-amount] }
       filter = SearchFilter.new(params)
+      value_slugs = filter.values
 
-      expect(filter.values.length).to eq(2)
-      expect(filter.values).to include('unknown')
-      expect(filter.values).to include('over-amount')
+      expect(value_slugs.length).to eq(2)
+      expect(value_slugs).to include('unknown')
+      expect(value_slugs).to include('over-amount')
     end
 
     it 'returns an array of value names' do
@@ -261,10 +276,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:value, name: 'over £100K',  slug: 'over-amount')
       params = { values: %w[unknown over-amount] }
       filter = SearchFilter.new(params)
+      value_names = filter.values(:name)
 
-      expect(filter.values(:name).length).to eq(2)
-      expect(filter.values(:name)).to include('Unknown')
-      expect(filter.values(:name)).to include('over £100K')
+      expect(value_names.length).to eq(2)
+      expect(value_names).to include('Unknown')
+      expect(value_names).to include('over £100K')
     end
 
     it 'returns an array of value data' do
@@ -287,10 +303,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:country, slug: 'australia')
       create(:country, slug: 'japan')
       params = { countries: %w[australia hackistan], regions: %w[north-east-asia] }
-      filters = SearchFilter.new(params)
+      filter = SearchFilter.new(params)
+      reduced_country_slugs = filter.reduced_countries
 
-      expect(filters.reduced_countries).to include('australia')
-      expect(filters.reduced_countries).not_to include('hackistan')
+      expect(reduced_country_slugs).to include('australia')
+      expect(reduced_country_slugs).not_to include('hackistan')
     end
 
     it 'returns an empty array when no country params' do
@@ -306,10 +323,11 @@ RSpec.describe SearchFilter, type: :service do
       create(:country, slug: 'japan')
       params = { countries: %w[france spain], regions: %w[north-east-asia] }
       filter = SearchFilter.new(params)
+      reduced_country_slugs = filter.reduced_countries
 
-      expect(filter.reduced_countries(:slug).length).to eq(2)
-      expect(filter.reduced_countries(:slug)).to include('france')
-      expect(filter.reduced_countries(:slug)).to include('spain')
+      expect(reduced_country_slugs.length).to eq(2)
+      expect(reduced_country_slugs).to include('france')
+      expect(reduced_country_slugs).to include('spain')
     end
 
     it 'returns an array of country names' do
@@ -347,13 +365,14 @@ RSpec.describe SearchFilter, type: :service do
       create(:country, slug: 'taiwan')
       params = { countries: %w[france spain], regions: %w[north-east-asia] }
       filter = SearchFilter.new(params)
+      reduced_country_slugs = filter.reduced_countries
 
-      expect(filter.reduced_countries.length).to eq(2)
-      expect(filter.reduced_countries).to include('france')
-      expect(filter.reduced_countries).to include('spain')
-      expect(filter.reduced_countries).to_not include('japan')
-      expect(filter.reduced_countries).to_not include('south-korea')
-      expect(filter.reduced_countries).to_not include('taiwan')
+      expect(reduced_country_slugs.length).to eq(2)
+      expect(reduced_country_slugs).to include('france')
+      expect(reduced_country_slugs).to include('spain')
+      expect(reduced_country_slugs).to_not include('japan')
+      expect(reduced_country_slugs).to_not include('south-korea')
+      expect(reduced_country_slugs).to_not include('taiwan')
     end
   end
 end
