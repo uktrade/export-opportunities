d5c12b103513a3a331c9fef9fb4607ea49b94f2b
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2018-12-18 10:32:23 +0000

    Add initial (not final) code for PTU email reminders

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -32,7 +32,10 @@ class EnquiryMailer < ApplicationMailer
     mail(args)
   end
 
-  def reminder(enquiry, number)
+  def reminder(enquiry, number, content)
+    # TODO: 
+    # What, if any, condition here?
+    # Should this have the excepted_service_providers condition, like send_enquiry?
     if enquiry.opportunity.contacts.present?
 
       # Convert number to required output format
@@ -40,21 +43,35 @@ class EnquiryMailer < ApplicationMailer
       reminder_number = number < reminder.length ? reminder[number].capitalize : "#{number}th"
 
       args = {
-               template_name: 'reminder',
-               to: enquiry.opportunity.contacts.pluck(:email),
-               subject: "#{reminder_number} reminder: respond to enquiry",
-             }
+        template_name: 'reminder',
+        to: enquiry.opportunity.contacts.pluck(:email), # Really? Or some other email?
+        subject: "#{reminder_number} reminder: respond to enquiry",
+      }
 
       if Figaro.env.enquiries_cc_email.present?
         args[:cc] = Figaro.env.enquiries_cc_email
       end
 
-      @content = get_content('opportunities/show.yml')
+      @content = content
       @enquiry = enquiry
       @opportunity = enquiry.opportunity
       @reminder_number = reminder_number
 
-      mail(args)
+      # Commented out just for development.
+      # This line needs to be active when ready.
+      # mail(args)
+    end
+
+    # Development only (remove when ready for above mail functionality)
+    puts '*** Development output... ***'
+    puts '*** Enable the mail() function to make this work properly ***'
+    puts "id: #{enquiry.id}, number: #{number}, reminder_number: #{reminder_number}"
+  end
+
+  def reminders(enquiry_reminders)
+    content = get_content('opportunities/show.yml')
+    enquiry_reminders.each do |reminder|
+      reminder(reminder[:enquiry], reminder[:number], content)
     end
   end
 end
