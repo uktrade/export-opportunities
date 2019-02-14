c69a2ed52d97aab2f35b96e358a0280ea5f57991
Will Taylor <wrftaylor@gmail.com>
2019-02-14 10:50:42 +0000

    Undo change - re-include Faraday

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,8 +1,20 @@
+require 'faraday_middleware/aws_signers_v4'
+
 # https://github.com/omniauth/omniauth/issues/872
 Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
-                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url)
+                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url) do |f|
+                                  f.request :aws_signers_v4,
+                                    credentials: Aws::Credentials.new(
+                                      Figaro.env.aws_access_key_id,
+                                      Figaro.env.aws_secret_access_key
+                                    ),
+                                    service_name: 'es',
+                                    region: Figaro.env.aws_region
+
+                                  f.adapter Faraday.default_adapter
+                                end
                               else
                                 Elasticsearch::Client.new host: '127.0.0.1:9200'
                               end
