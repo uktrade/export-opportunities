ac41521cc64607a2050e65e23a8fcf5423f051b8
alexandros giamas <agiamas@gmail.com>
2019-02-18 19:59:18 +0000

    (fix) re-undo elastic search change

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -2,17 +2,7 @@
 Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
-                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url) do |f|
-                                  f.request :aws_signers_v4,
-                                            credentials: Aws::Credentials.new(
-                                                Figaro.env.aws_access_key_id,
-                                                Figaro.env.aws_secret_access_key
-                                            ),
-                                            service_name: 'es',
-                                            region: Figaro.env.aws_region
-
-                                  f.adapter Faraday.default_adapter
-                                end
+                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url)
                               else
                                 Elasticsearch::Client.new host: '127.0.0.1:9200'
                               end
