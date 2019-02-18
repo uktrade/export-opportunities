3e40c69150caa88c34beff388eb30816faf18879
alex <agiamas@gmail.com>
2019-02-18 19:15:57 +0000

    (fix) rollback elastic_search_url (#485)

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -2,7 +2,7 @@
 Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
-                                Elasticsearch::Client.new(url: JSON.parse(ENV['VCAP_SERVICES'])["elasticsearch"][0]["credentials"]["uri"])
+                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url)
                               else
                                 Elasticsearch::Client.new host: '127.0.0.1:9200'
                               end
