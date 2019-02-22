d9e062a268bbcbb9bc392ab8d969e21889e62021
alexandros giamas <agiamas@gmail.com>
2019-02-22 15:49:03 +0000

    (fix) typo in edit

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -3,7 +3,6 @@ Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
                                 Elasticsearch::Client.new(url: Figaro.env.elastic_search_url)
-                              end
                               else
                                 Elasticsearch::Client.new host: '127.0.0.1:9200'
                               end
