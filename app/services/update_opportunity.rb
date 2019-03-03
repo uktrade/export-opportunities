2466228980525d363eb75ac4cdb3bc89eed46bbd
alexandros giamas <agiamas@gmail.com>
2019-03-03 19:38:25 +0000

    (fix) cpv ids to work with edit and delete cpvs

--- app/services/update_opportunity.rb
+++ app/services/update_opportunity.rb
@@ -18,18 +18,18 @@ class UpdateOpportunity
     @opportunity.slug = CreateOpportunitySlug.call(@opportunity)
     @opportunity.save
 
+    # delete existing cpv ids related to the opportunity
+    OpportunityCpv.where(opportunity_id: @opportunity.id).delete_all
+
+    # re-add the cpv ids that were passed in the params
     opportunity_cpv_ids_arr&.each do |opportunity_cpv|
       cpv_id = opportunity_cpv[:industry_id]
-      if OpportunityCpv.where(industry_id: cpv_id).where(opportunity_id: @opportunity.id).length.positive?
-        Rails.logger.debug "cpv id #{cpv_id} already exists for opportunity #{@opportunity.id}"
-      else
-        opportunity_cpv = OpportunityCpv.new(
-          industry_id: cpv_id.to_s,
-          industry_scheme: opportunity_cpv[:industry_scheme],
-          opportunity_id: @opportunity.id
-        )
-        opportunity_cpv.save!
-      end
+      opportunity_cpv = OpportunityCpv.new(
+        industry_id: cpv_id.to_s,
+        industry_scheme: opportunity_cpv[:industry_scheme],
+        opportunity_id: @opportunity.id
+      )
+      opportunity_cpv.save!
     end
 
     @opportunity
