2f60cd424a37cb0544ec92b2220fc86c9cc16c09
Steven Burnell <steve.burnel@digital.trade.gov.uk>
2019-04-04 16:17:11 +0100

    Fix inconsistent indentation

---
+++ app/assets/javascripts/transformation/dit.tagging.landing.js
@@ -0,0 +1,47 @@
+// GA360 Tagging Functions.
+// ---------------------
+
+// REQUIRES
+// jQuery
+
+dit.tagging.landing = (new function() {
+
+  // Page Tagging specific for landing
+  // Do not wrap in a function because we want
+  // this to run immediately on script load.
+  window.dataLayer.push({'pageCategory': 'LandingPage'});
+
+  // Event Tagging will be applied upon
+  // calling this function.
+  this.init = function() {
+    addTaggingForTopCTA();
+    addTaggingForBottomCTA();
+    addTaggingForFeaturedIndustries();
+  }
+
+
+  function addTaggingForTopCTA() {
+    $("#hero-banner .submit").on("click", function(e) {
+      window.dataLayer.push({'eventName': 'clicking on Export Opportunities landing page top CTA (above fold)'});
+      window.dataLayer.push({'eventID': 'landing_cta_search_hero'});
+    });
+  }
+
+  function addTaggingForBottomCTA() {
+    $("#auxiliary-search .submit").on("click", function(e) {
+      window.dataLayer.push({'eventName': 'clicking on Export Opportunities landing page bottom CTA (below fold)'});
+      window.dataLayer.push({'eventID': 'landing_cta_search_auxiliary'});
+    });
+  }
+
+  function addTaggingForFeaturedIndustries() {
+    $("#featured-industries a").on("click", function() {
+      var sector = this.href.replace(/.*?sectors\[\]=([\w]+)/, "$1");
+      window.dataLayer.push({
+        eventName: 'clicking on landing page featured industries',
+        eventID: sector
+      });
+    });
+  }
+
+});
