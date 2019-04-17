917831e23915be9e0a8270ae75d84187eb73bc42
Will <wrftaylor@gmail.com>
2019-04-17 11:01:03 +0100

    undo changes commited in 5326a42d6d1dce8bb16d65bbbfb902ca48b267f9 (#531)

--- app/controllers/email_notifications_controller.rb
+++ app/controllers/email_notifications_controller.rb
@@ -1,4 +1,5 @@
 class EmailNotificationsController < ApplicationController
+  before_action :require_sso! 
 
   def show
     content = get_content('email_notifications.yml')
--- spec/features/users_can_opt_out_of_emails_spec.rb
+++ spec/features/users_can_opt_out_of_emails_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 require 'capybara/email/rspec'
 
 RSpec.feature 'Users can opt out of emails' do
-  scenario 'by clicking a link in a feedback email', focus: true do
+  scenario 'by clicking a link in a feedback email' do
     user = create(:user, email: 'opt-out@example.com')
     enquiry = create(:enquiry, user: user)
-
+    mock_sso_with(email: 'opt-out@example.com')
+    
     EnquiryFeedbackSender.new.call(enquiry)
     open_email('opt-out@example.com')
     current_email.click_on 'Unsubscribe'
 
     expect(page).to have_content 'You will no longer receive requests for feedback'
-
-    # Expect confirmation email
-    raise
   end
 end
