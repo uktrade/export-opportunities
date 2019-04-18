c56fab01449bd23055a1e7ff6fce445ed7b9884b
Will Taylor <wrftaylor@gmail.com>
2019-04-18 11:38:03 +0100

    remove faraday_middleware

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -3,13 +3,13 @@ Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
                                 Elasticsearch::Client.new(url: Figaro.env.elastic_search_url) do |f|
-                                  f.request :aws_signers_v4,
-                                            credentials: Aws::Credentials.new(
-                                                Figaro.env.aws_access_key_id,
-                                                Figaro.env.aws_secret_access_key
-                                            ),
-                                            service_name: 'es',
-                                            region: Figaro.env.aws_region
+                                  # f.request :aws_signers_v4,
+                                  #           credentials: Aws::Credentials.new(
+                                  #               Figaro.env.aws_access_key_id,
+                                  #               Figaro.env.aws_secret_access_key
+                                  #           ),
+                                  #           service_name: 'es',
+                                  #           region: Figaro.env.aws_region
 
                                   f.adapter Faraday.default_adapter
                                 end
