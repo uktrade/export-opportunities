bc2aca9a04e171e9bd34a6cf78743d58174cd392
Will <wrftaylor@gmail.com>
2019-04-23 10:21:21 +0100

    Chore/last working version (#535)
    * update manifest
    
    * revert manifest to normal
    
    * upgrade rails to 5.2.2
    
    * remove faraday_middleware
    
    * add faraday_middleware sigv4 signer
    
    * add 'faraday_middleware/aws_sigv4' requirement
    
    * Only use ES in production
    
    * Tidy up Gemfile

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,15 +1,15 @@
+require 'faraday_middleware/aws_sigv4'
+
 # https://github.com/omniauth/omniauth/issues/872
 Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
                                 Elasticsearch::Client.new(url: Figaro.env.elastic_search_url) do |f|
-                                  f.request :aws_signers_v4,
-                                            credentials: Aws::Credentials.new(
-                                                Figaro.env.aws_access_key_id,
-                                                Figaro.env.aws_secret_access_key
-                                            ),
-                                            service: 'es',
-                                            region: Figaro.env.aws_region
+                                  f.request :aws_sigv4,
+                                             service: 'es',
+                                             access_key_id: Figaro.env.aws_access_key_id,
+                                             secret_access_key: Figaro.env.aws_secret_access_key,
+                                             region: Figaro.env.aws_region
 
                                   f.adapter Faraday.default_adapter
                                 end
