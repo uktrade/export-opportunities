5a7425799f4d3f2ead70eeb4b80fbb1f238e2b6c
Will Taylor <wrftaylor@gmail.com>
2019-04-30 09:57:42 +0100

    re-add SSO requirement to opt-out test spec

--- spec/features/users_can_opt_out_of_emails_spec.rb
+++ spec/features/users_can_opt_out_of_emails_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature 'Users can opt out of emails' do
   scenario 'by clicking a link in a feedback email' do
     user = create(:user, email: 'opt-out@example.com')
     enquiry = create(:enquiry, user: user)
+    mock_sso_with(email: 'opt-out@example.com')
     
     EnquiryFeedbackSender.new.call(enquiry)
     open_email('opt-out@example.com')
