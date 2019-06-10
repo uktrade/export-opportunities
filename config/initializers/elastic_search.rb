82c696e2df3d734a09e3ab3a6742ab19515122b4
Will <wrftaylor@gmail.com>
2019-06-10 16:35:48 +0100

    Chore/migrate elasticsearch to gov paas (#573)
    * Update Elasticsearch Credentials
    
    * add dynamic uri to elasticsearch credentials

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,18 +1,11 @@
-require 'faraday_middleware/aws_sigv4'
+require 'json'
 
 # https://github.com/omniauth/omniauth/issues/872
 Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
-                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url) do |f|
-                                  f.request :aws_sigv4,
-                                             service: 'es',
-                                             access_key_id: Figaro.env.aws_access_key_id,
-                                             secret_access_key: Figaro.env.aws_secret_access_key,
-                                             region: Figaro.env.aws_region
-
-                                  f.adapter Faraday.default_adapter
-                                end
+                                uri = JSON.parse(ENV['VCAP_SERVICES'])["elasticsearch"][0]["credentials"]["uri"]
+                                Elasticsearch::Client.new(url: uri)
                               else
                                 Elasticsearch::Client.new host: '127.0.0.1:9200'
                               end
