e38855a9b30e83fdd89da513b8b3d517f7906919
Will Taylor <wrftaylor@gmail.com>
2019-06-10 11:30:31 +0100

    add dynamic uri to elasticsearch credentials

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,8 +1,11 @@
+require 'json'
+
 # https://github.com/omniauth/omniauth/issues/872
 Hashie.logger = Logger.new(nil)
 
 Elasticsearch::Model.client = if Rails.env.production?
-                                Elasticsearch::Client.new(url: Figaro.env.elastic_search_url)
+                                uri = JSON.parse(ENV['VCAP_SERVICES'])["elasticsearch"][0]["credentials"]["uri"]
+                                Elasticsearch::Client.new(url: uri)
                               else
                                 Elasticsearch::Client.new host: '127.0.0.1:9200'
                               end
