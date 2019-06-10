04be24c4dd9ef0b686e307b4b1c42efe18c0dc7d
Will Taylor <wrftaylor@gmail.com>
2019-06-10 16:37:01 +0100

    added 'GMT' to timezone time

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -4,5 +4,5 @@ Time::DATE_FORMATS[:default] = '%e %B %Y %H:%M'
 Date::DATE_FORMATS[:long] = '%d %B %Y'
 
 Date::DATE_FORMATS[:admin_date] = '%d %h %Y'
-Time::DATE_FORMATS[:admin_datetime] = '%d %h %Y %l:%M %p %z'
-Time::DATE_FORMATS[:admin_history] = '%e %B %Y at %l:%M %p %z'
+Time::DATE_FORMATS[:admin_datetime] = '%d %h %Y %l:%M %p GMT %z'
+Time::DATE_FORMATS[:admin_history] = '%e %B %Y at %l:%M %p GMT %z'
