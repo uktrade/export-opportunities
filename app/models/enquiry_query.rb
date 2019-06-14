2c2b6fcc8a2bbf1704c897cbb41191583db08e30
Will <wrftaylor@gmail.com>
2019-06-14 15:58:28 +0100

    Feature/xot 589 add search to admin enquiries (#580)
    * add end to end tests for retireve volume opps
    
    * Add filter and column

--- app/models/enquiry_query.rb
+++ app/models/enquiry_query.rb
@@ -1,7 +1,8 @@
 class EnquiryQuery
   attr_reader :count, :enquiries
 
-  def initialize(scope: Enquiry.all, status: nil, filters: NullFilter.new, sort:, page: 1, per_page: 10)
+  def initialize(scope: Enquiry.all, status: nil, filters: NullFilter.new,
+      sort:, page: 1, per_page: 10, search_query: '', service_provider: '')
     @scope = scope
 
     @status = status
@@ -10,6 +11,8 @@ class EnquiryQuery
     @sort = sort
     @page = page
     @per_page = per_page
+    @search_query = search_query
+    @service_provider = service_provider
 
     @enquiries = fetch_enquiries
   end
@@ -38,6 +41,16 @@ class EnquiryQuery
       elsif @status == 'not_replied'
         query = query.joins('left outer join enquiry_responses on enquiry_responses.enquiry_id=enquiries.id') unless @sort.column == 'opportunity'
         query = query.where('enquiry_responses.id is null or completed_at is null')
+      end
+
+      if @search_query.present?
+        query = query.joins(:opportunity).where("opportunities.title LIKE ? OR
+          company_name LIKE ?","%#{@search_query}%", "%#{@search_query}%")
+      end
+
+      if @service_provider.present?
+        sp = ServiceProvider.where("name LIKE ?", "%#{@service_provider}%")
+        query = query.joins(:opportunity).where(opportunities: { service_provider: sp })
       end
 
       # Secondary sort order to prevent pagination weirdness
