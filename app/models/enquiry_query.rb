88ebb44219931a7a78474e85cb936892d98392e0
Will Taylor <wrftaylor@gmail.com>
2019-06-19 16:22:13 +0100

    remove merge conflict

--- app/models/enquiry_query.rb
+++ app/models/enquiry_query.rb
@@ -44,21 +44,12 @@ class EnquiryQuery
       end
 
       if @search_query.present?
-<<<<<<< HEAD
         query = query.joins(:opportunity).where('LOWER(opportunities.title) LIKE LOWER(?) OR
           LOWER(company_name) LIKE LOWER(?)', "%#{@search_query}%", "%#{@search_query}%")
       end
 
       if @service_provider.present?
         sp = ServiceProvider.where('LOWER(name) LIKE LOWER(?)', "%#{@service_provider}%")
-=======
-        query = query.joins(:opportunity).where("opportunities.title LIKE ? OR
-          company_name LIKE ?","%#{@search_query}%", "%#{@search_query}%")
-      end
-
-      if @service_provider.present?
-        sp = ServiceProvider.where("name LIKE ?", "%#{@service_provider}%")
->>>>>>> master
         query = query.joins(:opportunity).where(opportunities: { service_provider: sp })
       end
 
