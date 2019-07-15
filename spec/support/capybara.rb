2b02d49c5aa8c0b3a8b8f9444e83ac9342c2ce9e
Will Taylor <wrftaylor@gmail.com>
2019-07-15 16:23:46 +0100

    update phantomjs config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,9 @@
 require 'capybara/poltergeist'
 
+# Note, phantomjs uses this solution
+# https://github.com/teampoltergeist/poltergeist/issues/539
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--load-images=false'])
+  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--load-images=false'], phantomjs: Phantomjs.path)
 end
 Capybara.javascript_driver = :poltergeist
 Capybara.ignore_hidden_elements = true
