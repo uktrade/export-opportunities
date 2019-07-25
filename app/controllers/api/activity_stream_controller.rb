cf51af5fad355ee1c1af0227ed89a57252c5bc5b
Will <wrftaylor@gmail.com>
2019-07-25 12:03:13 +0100

    update activity stream controller (#598)

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -172,8 +172,7 @@ module Api
       def opportunity_object(country_names, service_provider_names, opportunity)
         obj_id = 'dit:exportOpportunities:Opportunity:' + opportunity.id.to_s
         {
-          'type': 'Opportunity',
-          'dit:greatDomesticUI:searchResultType': 'Opportunity',
+          'type': ['Document', 'Opportunity', 'dit:Opportunity'],
           # The following is used by Enquiry stream, may be deprecated soon - 11 Feb 2019
           'dit:exportOpportunities:Opportunity:id': opportunity.id.to_s,
           'id': obj_id,
