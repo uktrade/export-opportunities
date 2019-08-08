f70fa03d42f8c80e86a9310e7435d2c960fe3f8b
Will Taylor <wrftaylor@gmail.com>
2019-08-08 12:15:44 +0100

    added initial implementation of hawk utility

---
+++ app/services/hawk.rb
@@ -0,0 +1,140 @@
+require 'securerandom'
+
+module Hawk
+  extend self
+
+  REQUIRED_OPTIONS = [:method, :request_uri, :host, :port].freeze
+  REQUIRED_CREDENTIAL_MEMBERS = [:id, :key, :algorithm].freeze
+  SUPPORTED_ALGORITHMS = ['sha256', 'sha1'].freeze
+  HEADER_PARTS = [:id, :ts, :nonce, :hash, :ext, :mac].freeze
+
+  DEFAULT_TIMESTAMP_SKEW = 60.freeze # ±60 seconds
+
+  MissingOptionError = Class.new(StandardError)
+  InvalidCredentialsError = Class.new(StandardError)
+  InvalidAlgorithmError = Class.new(StandardError)
+
+  def authenticate(authorization_header, options)
+    big_authenticate(authorization_header, { :server_response => true, :type => 'response' }.merge(options))
+  end
+
+  def build_authorization_header(options)
+    build(options)
+  end
+
+  def calculate_time_offset(authorization_header, options)
+    parts = parse(authorization_header)
+    expected_mac = Crypto.ts_mac(:ts => parts[:ts], :credentials => options[:credentials]).to_s
+    return unless expected_mac == parts[:tsm]
+    parts[:ts].to_i - Time.now.to_i
+  end
+
+  def build(options, only=nil)
+    options[:ts] ||= Time.now.to_i
+    options[:nonce] ||= SecureRandom.hex(4)
+
+    REQUIRED_OPTIONS.each do |key|
+      unless options.has_key?(key)
+        raise MissingOptionError.new("#{key.inspect} is missing!")
+      end
+    end
+
+    credentials = options[:credentials]
+    REQUIRED_CREDENTIAL_MEMBERS.each do |key|
+      unless credentials.has_key?(key)
+        raise InvalidCredentialsError.new("#{key.inspect} is missing!")
+      end
+    end
+
+    unless SUPPORTED_ALGORITHMS.include?(credentials[:algorithm])
+      raise InvalidAlgorithmError.new("#{credentials[:algorithm].inspect} is not a supported algorithm! Use one of the following: #{SUPPORTED_ALGORITHMS.join(', ')}")
+    end
+
+    hash = Crypto.hash(options).to_s
+    mac = Crypto.mac(options)
+
+    parts = {
+      :id => credentials[:id],
+      :ts => options[:ts],
+      :nonce => options[:nonce],
+      :mac => mac.to_s
+    }
+    parts[:hash] = hash if options.has_key?(:payload) && !options[:payload].nil?
+    parts[:ext] = options[:ext] if options.has_key?(:ext)
+
+    "Hawk " << (only || HEADER_PARTS).inject([]) { |memo, key|
+      next memo unless parts.has_key?(key)
+      memo << %(#{key}="#{parts[key]}")
+      memo
+    }.join(', ')
+  end
+
+  def big_authenticate(header, options)
+    options = options.dup
+
+    parts = parse(header)
+    options.delete(:payload) unless parts[:hash]
+
+    now = Time.now.to_i
+
+    options[:timestamp_skew] ||= DEFAULT_TIMESTAMP_SKEW
+
+    if options[:server_response]
+      credentials = options[:credentials]
+      parts.merge!(
+        :ts => options[:ts],
+        :nonce => options[:nonce]
+      )
+    else
+      unless options[:credentials_lookup].respond_to?(:call) && (credentials = options[:credentials_lookup].call(parts[:id]))
+        return AuthenticationFailure.new(:id, "Unidentified id")
+      end
+
+      if (now - parts[:ts].to_i > options[:timestamp_skew]) || (parts[:ts].to_i - now > options[:timestamp_skew])
+        # Stale timestamp
+        return AuthenticationFailure.new(:ts, "Stale ts", :credentials => credentials)
+      end
+
+      unless parts[:nonce]
+        return AuthenticationFailure.new(:nonce, "Missing nonce")
+      end
+
+      if options[:nonce_lookup].respond_to?(:call) && options[:nonce_lookup].call(parts[:nonce])
+        # Replay
+        return AuthenticationFailure.new(:nonce, "Invalid nonce")
+      end
+    end
+
+    mac_opts = options.merge(
+      :credentials => credentials,
+      :ts => parts[:ts],
+      :nonce => parts[:nonce],
+      :ext => parts[:ext],
+      :app => options[:app] || parts[:app],
+      :dlg => options[:dlg] || parts[:dlg]
+    )
+
+    expected_hash = parts[:hash] ? Crypto.hash(options.merge(:credentials => credentials)) : nil
+    if expected_hash && expected_hash.to_s != parts[:hash]
+      return AuthenticationFailure.new(:hash, "Invalid hash. #{expected_hash.normalized_string}")
+    end
+
+    expected_mac = Crypto.mac(mac_opts)
+    unless expected_mac.eql?(parts[:mac])
+      return AuthenticationFailure.new(:mac, "Invalid mac. #{expected_mac.normalized_string}")
+    end
+
+    credentials
+  end
+
+  def parse(header)
+    parts = header.sub(/\AHawk\s+/, '').split(/,\s*/)
+    parts.inject(Hash.new) do |memo, part|
+      next memo unless part =~ %r{([a-z]+)=(['"])([^\2]+)\2}
+      key, val = $1, $3
+      memo[key.to_sym] = val
+      memo
+    end
+  end
+
+end
---
+++ app/services/hawk_utility.rb
@@ -0,0 +1,52 @@
+require 'uri'
+
+class HawkUtility
+  #
+  # This general class is designed to allow you to manually test
+  # any Hawk authenticated activity stream endpoint
+  # Note all AS endpoints require requests to port 443
+  #
+
+  def initialize
+    @sender_id ='directory'
+    @secret ='debug'
+    @host = 'http://api.trade.great'
+    @port = '8000'
+    @base_url = 'http://api.trade.great:8000'
+    @path = '/public/company/10000000/'
+    @url = @base_url + @path
+    @version = "14.0.0"
+    @method='GET'
+    @body=''
+    @content_type='text/plain'
+  end
+
+  def request
+    response = Faraday.get do |request|
+      request.url url
+      request.headers["User-agent"] = "EXPORT-DIRECTORY-API-CLIENT/#{@version}"
+      request.headers['Content-Type'] = @content_type
+      request.headers['X-Signature'] = get_header
+      request.body = @body
+    end
+    response.body
+  end
+
+  def get_header
+    Hawk.build_authorization_header(
+      credentials: {
+        id: @sender_id,
+        key: @secret,
+        algorithm: 'sha256',
+      },
+      method: 'GET',
+      host: @host,
+      port: @port,
+      request_uri: @url,
+      content_type: @content_type,
+      payload: @body
+    )
+  end
+
+end
+# HawkUtility.new.get_header
