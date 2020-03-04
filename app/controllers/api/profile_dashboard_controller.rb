722b37d984745fdcde738e0ccda722556864a208
Will Taylor <wrftaylor@gmail.com>
2020-03-04 12:54:58 +0000

    Reduce complexity

--- app/controllers/api/profile_dashboard_controller.rb
+++ app/controllers/api/profile_dashboard_controller.rb
@@ -7,35 +7,15 @@ module Api
       hashed_sso_id = params[:hashed_sso_id] || params[:sso_user_id]
       return bad_request! unless hashed_sso_id && params[:shared_secret]
       return forbidden! if params[:shared_secret] != Figaro.env.api_profile_dashboard_shared_secret
-
-      @result = { status: 'ok', code: 200, enquiries: [], email_alerts: [] }
-
       user = User.find_by(sso_hashed_uuid: hashed_sso_id)
       return forbidden! if user.nil?
 
+      @result = { status: 'ok', code: 200, enquiries: [], email_alerts: [] }
       @enquiries = user.enquiries.includes(:opportunity)
-      @result[:enquiries] = @enquiries.map do |enq|
-        {
-          title: enq.opportunity.title,
-          opportunity_url: opportunity_url(enq.opportunity.slug),
-          description: enq.opportunity.description,
-          submitted_on: enq.created_at,
-          expiration_date: enq.opportunity.response_due_on,
-          enquiry_url: dashboard_enquiry_url(enq.id),
-        }
-      end
-
+      @result[:enquiries] = result_enquiries(@enquiries)
       @subscriptions = user.subscriptions.includes(:types, :values, :countries, :sectors).active
-
-      @result[:email_alerts] = @subscriptions.map do |sub|
-        {
-          term: sub.search_term,
-          created_on: sub.created_at,
-          unsubscribe_url: unsubscribe_url(sub.id),
-          title: sub.title,
-          countries: sub.countries.map(&:slug),
-        }
-      end
+      @result[:email_alerts] = result_subscriptions(@subscriptions)
+      
       respond_to do |format|
         format.json { render status: :ok, json: @result }
       end
@@ -68,6 +48,31 @@ module Api
     end
 
     private
+
+      def result_enquiries(enquiries)
+        enquiries.map do |enq|
+          {
+            title: enq.opportunity.title,
+            opportunity_url: opportunity_url(enq.opportunity.slug),
+            description: enq.opportunity.description,
+            submitted_on: enq.created_at,
+            expiration_date: enq.opportunity.response_due_on,
+            enquiry_url: dashboard_enquiry_url(enq.id),
+          }
+        end
+      end
+
+      def result_subscriptions(subscriptions)
+        subscriptions.map do |sub|
+          {
+            term: sub.search_term,
+            created_on: sub.created_at,
+            unsubscribe_url: unsubscribe_url(sub.id),
+            title: sub.title,
+            countries: sub.countries.map(&:slug),
+          }
+        end
+      end
 
       def bad_request!
         render status: :bad_request, json: { status: 'Bad Request', code: 400 }.freeze
