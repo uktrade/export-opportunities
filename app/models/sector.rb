60798bbfb966d4f21511d0ba0e40be4c3085b788
Will Taylor <wrftaylor@gmail.com>
2020-04-06 15:51:49 +0100

    Update database settings

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,7 +1,7 @@
 class Sector < ApplicationRecord
   scope :featured, -> { where(featured: true).order(:featured_order) }
 
-  scope :visible, -> { where.not(hidden: true) }
+  scope :visible, -> { where(hidden: [nil, false]) }
 
   def to_param
     slug
