2588381f2c3352af12d14c30798402c06c0a2d8d
Will <wrftaylor@gmail.com>
2020-05-01 15:52:55 +0100

    Fix bug causing unsubscriptions to fail (#714)
    * Fix bug causing unsubscriptions to fail
    
    * fix issue with CCI breaking due to missing Env Var
    
    * move to unsubscription tokens
    
    * Add backwards compatability

--- spec/features/users_can_view_opportunity_notification_emails_spec.rb
+++ spec/features/users_can_view_opportunity_notification_emails_spec.rb
@@ -8,11 +8,25 @@ feature "User viewing opportunity notification email" do
 
     OpportunityMailer.send_opportunity(user, {count: 2, subscriptions: {}}).deliver_now!
 
+    email = ActionMailer::Base.deliveries.last
+    path = ENV["DOMAIN"] + delete_email_notifications_path(user.unsubscription_token)
+    assert email.html_part.body.include?(path)
+    assert email.text_part.body.include?(path)
+    visit(path)
+    subscription.reload
+    assert subscription.unsubscribed_at != nil
+  end
+
+  scenario 'can unsubscribed from with old encrypted ID method', focus: true do
+    user = create(:user, email: 'test@example.com')
+    subscription = create(:subscription, user: user)
+    assert_nil subscription.unsubscribed_at
+
+    OpportunityMailer.send_opportunity(user, {count: 2, subscriptions: {}}).deliver_now!
+
     encrypted_id = EncryptedParams.encrypt(user.id)
     email = ActionMailer::Base.deliveries.last
     path = ENV["DOMAIN"] + delete_email_notifications_path(encrypted_id)
-    assert email.html_part.body.include?(path)
-    assert email.text_part.body.include?(path)
     visit(path)
     subscription.reload
     assert subscription.unsubscribed_at != nil
