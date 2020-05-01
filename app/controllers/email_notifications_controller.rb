818ed5d17db96cccb7ad7fbb8409a8949f0197be
WillTaylor22 <wrftaylor@gmail.com>
2020-05-01 14:07:17 +0100

    Add backwards compatability

--- app/controllers/email_notifications_controller.rb
+++ app/controllers/email_notifications_controller.rb
@@ -7,6 +7,17 @@ class EmailNotificationsController < ApplicationController
   def destroy
     content = get_content('email_notifications.yml')
     user = User.where(unsubscription_token: params[:unsubscription_token]).first
+    
+    unless user
+      # In old method, the params[:unsubscription_token] received will
+      # actually be the user_id
+      begin
+        user_id = EncryptedParams.decrypt(params[:unsubscription_token])
+      rescue EncryptedParams::CouldNotDecrypt
+        redirect_to not_found && return
+      end
+      user = User.where(id: user_id).first
+    end
 
     unless user
       flash[:error] = "Could not find user"
--- spec/features/users_can_view_opportunity_notification_emails_spec.rb
+++ spec/features/users_can_view_opportunity_notification_emails_spec.rb
@@ -16,4 +16,19 @@ feature "User viewing opportunity notification email" do
     subscription.reload
     assert subscription.unsubscribed_at != nil
   end
+
+  scenario 'can unsubscribed from with old encrypted ID method', focus: true do
+    user = create(:user, email: 'test@example.com')
+    subscription = create(:subscription, user: user)
+    assert_nil subscription.unsubscribed_at
+
+    OpportunityMailer.send_opportunity(user, {count: 2, subscriptions: {}}).deliver_now!
+
+    encrypted_id = EncryptedParams.encrypt(user.id)
+    email = ActionMailer::Base.deliveries.last
+    path = ENV["DOMAIN"] + delete_email_notifications_path(encrypted_id)
+    visit(path)
+    subscription.reload
+    assert subscription.unsubscribed_at != nil
+  end
 end
