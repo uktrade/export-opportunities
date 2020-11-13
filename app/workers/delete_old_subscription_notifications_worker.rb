a6b48dcb0804baf4a4d33760261f89d3f83cc039
Rafa Garcia <rafa@rafagarcia.info>
2020-11-13 09:44:58 +0000

    Fix bug on DeleteOldSubscriptionNotificationsWorker

--- app/workers/delete_old_subscription_notifications_worker.rb
+++ app/workers/delete_old_subscription_notifications_worker.rb
@@ -4,6 +4,6 @@ class DeleteOldSubscriptionNotificationsWorker
   sidekiq_options retry: false
 
   def perform
-    SubscriptionNotification.where('created_at > ?', 30.days.ago).destroy_all
+    SubscriptionNotification.where('created_at < ?', 1.month.ago).delete_all
   end
 end
