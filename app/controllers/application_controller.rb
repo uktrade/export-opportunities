5410a566739df8d97bfc4ffb47943bb445de6c16
Rafa Garcia <rafa@rafagarcia.info>
2021-05-27 16:17:07 +0100

    Bypass sso parity on API endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -109,7 +109,7 @@ class ApplicationController < ActionController::Base
   before_action :force_sign_in_parity
   def force_sign_in_parity
     # A sso session cookie must be present to qualify for sign-in – users only
-    unless request.path.include?('/admin')
+    unless request.path.include?('/admin') || request.path.include?('/api')
       sign_out current_user unless cookies[sso_session_cookie]
     end
 
