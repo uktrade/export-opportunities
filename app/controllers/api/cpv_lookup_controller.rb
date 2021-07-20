283f9cfb6d50283bf78f7f34fb9d5098afcebf9c
Rafa Garcia <rafa@rafagarcia.info>
2021-07-20 07:10:08 +0100

    Add new endpoint for CPV lookup

---
+++ app/controllers/api/cpv_lookup_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Api
+  # CpvLookupController
+  class CpvLookupController < ApplicationController
+    skip_before_action :verify_authenticity_token, raise: false
+
+    def search
+      return not_found unless (term = params[:description])
+
+      results = Cn2019.search(query(term)).records
+
+      respond_to do |format|
+        format.json { render status: :ok, json: results }
+      end
+    end
+
+    private
+
+    def not_found
+      render status: :not_found, json: { 'detail': 'Not found' }
+    end
+
+    def query(term)
+      {
+        'size': 100,
+        'query': {
+          'simple_query_string': {
+            'query': term,
+            'fields': ['description^5', 'english_text', 'parent_description']
+          }
+        }
+      }
+    end
+  end
+end
---
+++ test/models/cn2019_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Cn2019Test < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
