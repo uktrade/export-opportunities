e849a158de29f98e9b37a531a57265ffc555cd90
Sebastian Lees <sebastian.david.lees@digital.trade.gov.uk>
2024-07-12 08:35:38 +0100

    FIXED CSRF Error in Document Upload.

--- app/controllers/api/document_controller.rb
+++ app/controllers/api/document_controller.rb
@@ -2,6 +2,7 @@ require 'digest'
 
 module Api
   class DocumentController < ApplicationController
+    skip_before_action :verify_authenticity_token, raise: false
 
     def index
       params[:id].to_i
