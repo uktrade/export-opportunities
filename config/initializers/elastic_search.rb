458443c09720e331cdc8e7beefab2f31a6faf39d
Rafa Garcia <rafa@rafagarcia.info>
2024-08-08 08:44:34 +0100

    Load specific platform Opensearch config

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -3,10 +3,16 @@ require 'json'
 # https://github.com/omniauth/omniauth/issues/872
 Hashie.logger = Logger.new(nil)
 
-service_name = ENV.fetch('ES_SERVICE', 'opensearch')
-Elasticsearch::Model.client = if Rails.env.production?
-                                uri = JSON.parse(ENV['VCAP_SERVICES'])[service_name][0]["credentials"]["uri"]
-                                Elasticsearch::Client.new(url: uri)
-                              else
-                                Elasticsearch::Client.new host: '127.0.0.1:9200'
-                              end
+if Rails.env.production?
+  if ENV['COPILOT_ENVIRONMENT_NAME'] # DBT Platform
+    kwargs = { url: ENV.fetch('OPENSEARCH_URL') }
+  elsif ENV['VCAP_SERVICES'] # Govt PaaS / Clout Foundry Platform
+    kwargs = { url: JSON.parse(ENV['VCAP_SERVICES'])['opensearch'][0]["credentials"]["uri"] }
+  else
+    raise Exception, 'Platform type not identified'
+  end
+else
+  kwargs = { host: '127.0.0.1:9200' }
+end
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(kwargs)
