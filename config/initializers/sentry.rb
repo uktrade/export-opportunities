b504d7fddaff1da7341cf1ed8d7681c56e5910d5
Kudrotey <kudrotey.khuda@digital.trade.gov.uk>
2024-12-09 10:47:33 +0000

    chore: enable sentry tracing.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Sentry.init do |config|
   config.dsn = Figaro.env.SENTRY_RAVEN_DSN
-  config.enabled_environments = %w[staging production]
+  config.enabled_environments = %w[dev staging uat prod production]
+  config.traces_sample_rate = 1.0
 end
