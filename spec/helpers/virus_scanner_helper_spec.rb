64a5c4d9dd072f03291a759a70aeb3e86a0bcb65
Rafa Garcia <rafa@rafagarcia.info>
2025-02-28 13:04:55 +0000

    Update ClamAV interface

--- spec/helpers/virus_scanner_helper_spec.rb
+++ spec/helpers/virus_scanner_helper_spec.rb
@@ -6,38 +6,36 @@ describe VirusScannerHelper do
   describe '#virus scanner with local file' do
     it 'scan an infected file' do
       result = scan_clean_by_file_path?('spec/files/tender_sample_infected_file.txt')
-      expect(result['malware']).to eq(true)
-      expect(result['reason']).to eq('Eicar-Signature')
+      expect(result).to eq('NOTOK')
     end
 
     it 'scan a clean file' do
       skip('fixed in develop')
       result = scan_clean_by_file_path?('spec/files/tender_sample_file.txt')
-      expect(result['malware']).to eq(false)
+      expect(result).to eq('OK')
     end
   end
 
   describe '#virus scanner with file stream' do
     it 'scan an infected file' do
       result = scan_clean('test_infected_file', File.open('spec/files/tender_sample_infected_file.txt', 'rb'))
-      expect(result['malware']).to eq(true)
+      expect(result).to eq('NOTOK')
     end
 
     it 'scan a clean file' do
       result = scan_clean('test_clean_file', File.open('spec/files/tender_sample_file.txt', 'rb'))
-      expect(result['malware']).to eq(false)
+      expect(result).to eq('OK')
     end
 
     it 'scan a clean zip file' do
       result = scan_clean('test_clean_file', File.open('spec/files/tender_sample_file.zip', 'rb'))
-      expect(result['malware']).to eq(false)
+      expect(result).to eq('OK')
     end
 
     it 'scan a clean zip pw protected file' do
       # Password protected should return "has a virus"
       result = scan_clean('test_clean_file', File.open('spec/files/tender_sample_file_pw.zip', 'rb'))
-      expect(result['malware']).to eq(true)
-      expect(result['reason']).to eq('Heuristics.Encrypted.Zip')
+      expect(result).to eq('NOTOK')
     end
   end
 end
