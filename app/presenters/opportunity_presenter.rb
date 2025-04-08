ccdc189be43090f18a1fcecaa805881ca060c702
George <george.schena@digital.trade.gov.uk>
2025-04-08 12:52:33 +0100

    Rename function

--- app/presenters/opportunity_presenter.rb
+++ app/presenters/opportunity_presenter.rb
@@ -95,7 +95,7 @@ class OpportunityPresenter < PagePresenter
     links = []
     if guides.length > 5
       links.push(h.link_to('Country guides',
-                           "https://www.#{is_bgs_site?}/markets/",
+                           "https://www.#{fetch_domain}/markets/",
                            target: '_blank', rel: 'noopener noreferrer', title: 'Opens in a new window'))
     else
       guides.each do |country|
