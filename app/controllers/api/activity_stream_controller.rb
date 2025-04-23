e61cb749d159f7e9d0c5c7e50acfbd16f4eadeed
George <george.schena@digital.trade.gov.uk>
2025-04-23 17:07:13 +0100

    Add created_at to csat_feedback_object

--- app/controllers/api/activity_stream_controller.rb
+++ app/controllers/api/activity_stream_controller.rb
@@ -211,6 +211,7 @@ module Api
 
         {
           'id': csat_feedback.id,
+          'created_at': csat_feedback.created_at.to_datetime.rfc3339,
           'type': prefix,
           'url': csat_feedback.url,
           'user_journey': csat_feedback.user_journey,
