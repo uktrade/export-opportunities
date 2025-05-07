967b5dae2d33925595ab8b398554ca9c312915d7
George <george.schena@digital.trade.gov.uk>
2025-05-07 10:44:24 +0100

    Ensure we support hotfix branch for domain

--- app/helpers/domain_helper.rb
+++ app/helpers/domain_helper.rb
@@ -2,11 +2,17 @@
 
 # Helper module for domain-related functionality
 module DomainHelper
+  HOTFIX_DOMAIN = 'www.hotfix.bgs.uktrade.digital'
+
   def bgs_site?
     Figaro.env.DOMAIN == Figaro.env.BGS_SITE
   end
 
   def fetch_domain(title_only: false)
+    if bgs_site? && Figaro.env.DOMAIN == HOTFIX_DOMAIN
+      return 'hotfix.bgs.uktrade.digital'
+    end
+
     domains = {
       bgs: { title: 'Business', domain: 'business.gov.uk' },
       great: { title: 'Great', domain: 'great.gov.uk' }
